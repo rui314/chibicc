6eb2c2b4ee7d4c83a9e1fb2700c0be38a6147f63
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-03 17:17:13 +0900

    Split main.c into multiple small files

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CFLAGS=-std=c11 -g -static -fno-common
+SRCS=$(wildcard *.c)
+OBJS=$(SRCS:.c=.o)
 
-chibicc: main.o
-	$(CC) -o $@ $? $(LDFLAGS)
+chibicc: $(OBJS)
+	$(CC) -o $@ $(OBJS) $(LDFLAGS)
+
+$(OBJS): chibi.h
 
 test: chibicc
 	./test.sh
---
+++ parse.c
@@ -0,0 +1,113 @@
+#include "chibi.h"
+
+static Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+static Node *new_num(long val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+static Node *equality(void);
+static Node *relational(void);
+static Node *add(void);
+static Node *mul(void);
+static Node *unary(void);
+static Node *primary(void);
+
+// expr = equality
+Node *expr(void) {
+  return equality();
+}
+
+// equality = relational ("==" relational | "!=" relational)*
+static Node *equality(void) {
+  Node *node = relational();
+
+  for (;;) {
+    if (consume("=="))
+      node = new_binary(ND_EQ, node, relational());
+    else if (consume("!="))
+      node = new_binary(ND_NE, node, relational());
+    else
+      return node;
+  }
+}
+
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+static Node *relational(void) {
+  Node *node = add();
+
+  for (;;) {
+    if (consume("<"))
+      node = new_binary(ND_LT, node, add());
+    else if (consume("<="))
+      node = new_binary(ND_LE, node, add());
+    else if (consume(">"))
+      node = new_binary(ND_LT, add(), node);
+    else if (consume(">="))
+      node = new_binary(ND_LE, add(), node);
+    else
+      return node;
+  }
+}
+
+// add = mul ("+" mul | "-" mul)*
+static Node *add(void) {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume("+"))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume("-"))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+// mul = unary ("*" unary | "/" unary)*
+static Node *mul(void) {
+  Node *node = unary();
+
+  for (;;) {
+    if (consume("*"))
+      node = new_binary(ND_MUL, node, unary());
+    else if (consume("/"))
+      node = new_binary(ND_DIV, node, unary());
+    else
+      return node;
+  }
+}
+
+// unary = ("+" | "-")? unary
+//       | primary
+static Node *unary(void) {
+  if (consume("+"))
+    return unary();
+  if (consume("-"))
+    return new_binary(ND_SUB, new_num(0), unary());
+  return primary();
+}
+
+// primary = "(" expr ")" | num
+static Node *primary(void) {
+  if (consume("(")) {
+    Node *node = expr();
+    expect(")");
+    return node;
+  }
+
+  return new_num(expect_number());
+}
