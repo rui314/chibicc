c6ff1d98a1419e69c31902447e2caa85af4e9844
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-03 15:02:08 +0900

    Improve error message
    Now, chibicc can print out an error message with an error location
    like this:
    
      $ ./chibicc 1+foo
      1+foo
        ^ expected a number

--- main.c
+++ main.c
@@ -20,6 +20,9 @@ struct Token {
   char *str;      // Token string
 };
 
+// Input program
+char *user_input;
+
 // Current token
 Token *token;
 
@@ -27,6 +30,20 @@ Token *token;
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// Reports an error location and exit.
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // print pos spaces.
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
@@ -43,14 +60,14 @@ bool consume(char op) {
 // Ensure that the current token is `op`.
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("expected '%c'", op);
+    error_at(token->str, "expected '%c'", op);
   token = token->next;
 }
 
 // Ensure that the current token is TK_NUM.
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("expected a number");
+    error_at(token->str, "expected a number");
   int val = token->val;
   token = token->next;
   return val;
@@ -69,8 +86,9 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
   return tok;
 }
 
-// Tokenize `p` and returns new tokens.
-Token *tokenize(char *p) {
+// Tokenize `user_input` and returns new tokens.
+Token *tokenize() {
+  char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
@@ -95,7 +113,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("invalid token");
+    error_at(p, "expected a number");
   }
 
   new_token(TK_EOF, cur, p);
@@ -108,7 +126,8 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  token = tokenize(argv[1]);
+  user_input = argv[1];
+  token = tokenize();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
