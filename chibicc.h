30a39926272a8341c52018654ca18d2c86ba662b
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-04 18:25:20 +0900

    Support zero-arity function calls

--- chibicc.h
+++ chibicc.h
@@ -80,6 +80,7 @@ typedef enum {
   ND_IF,        // "if"
   ND_FOR,       // "for" or "while"
   ND_BLOCK,     // { ... }
+  ND_FUNCALL,   // Function call
   ND_EXPR_STMT, // Expression statement
   ND_VAR,       // Variable
   ND_NUM,       // Integer
@@ -104,6 +105,9 @@ struct Node {
 
   // Block
   Node *body;
+
+  // Function call
+  char *funcname;
 
   Obj *var;      // Used if kind == ND_VAR
   int val;       // Used if kind == ND_NUM
