1f3eb34f637520b01e6b8cd10a9026d05036db6d
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-04 17:24:03 +0900

    Add "while" statement

--- chibicc.h
+++ chibicc.h
@@ -73,7 +73,7 @@ typedef enum {
   ND_ASSIGN,    // =
   ND_RETURN,    // "return"
   ND_IF,        // "if"
-  ND_FOR,       // "for"
+  ND_FOR,       // "for" or "while"
   ND_BLOCK,     // { ... }
   ND_EXPR_STMT, // Expression statement
   ND_VAR,       // Variable
--- tokenize.c
+++ tokenize.c
@@ -80,7 +80,7 @@ static int read_punct(char *p) {
 }
 
 static bool is_keyword(Token *tok) {
-  static char *kw[] = {"return", "if", "else", "for"};
+  static char *kw[] = {"return", "if", "else", "for", "while"};
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
     if (equal(tok, kw[i]))
