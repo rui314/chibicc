611e5d55a3f168fcb474110a22ac41c36ce01423
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-04 17:24:03 +0900

    Add "while" statement

--- parse.c
+++ parse.c
@@ -65,6 +65,7 @@ static Obj *new_lvar(char *name) {
 // stmt = "return" expr ";"
 //      | "if" "(" expr ")" stmt ("else" stmt)?
 //      | "for" "(" expr-stmt expr? ";" expr? ")" stmt
+//      | "while" "(" expr ")" stmt
 //      | "{" compound-stmt
 //      | expr-stmt
 static Node *stmt(Token **rest, Token *tok) {
@@ -100,6 +101,15 @@ static Node *stmt(Token **rest, Token *tok) {
       node->inc = expr(&tok, tok);
     tok = skip(tok, ")");
 
+    node->then = stmt(rest, tok);
+    return node;
+  }
+
+  if (equal(tok, "while")) {
+    Node *node = new_node(ND_FOR);
+    tok = skip(tok->next, "(");
+    node->cond = expr(&tok, tok);
+    tok = skip(tok, ")");
     node->then = stmt(rest, tok);
     return node;
   }
--- test.sh
+++ test.sh
@@ -72,4 +72,6 @@ assert 3 '{ if (1) { 1; 2; return 3; } else { return 4; } }'
 assert 55 '{ i=0; j=0; for (i=0; i<=10; i=i+1) j=i+j; return j; }'
 assert 3 '{ for (;;) {return 3;} return 5; }'
 
+assert 10 '{ i=0; while(i<10) { i=i+1; } return i; }'
+
 echo OK
