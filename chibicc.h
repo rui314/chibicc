6364dcc7b0ad0ac1cb28f570cd06a58c66eacf8b
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-07 08:05:18 +0900

    [GNU] Add statement expression
    This is a GNU C extension but will be useful for writing tests.

--- chibicc.h
+++ chibicc.h
@@ -91,6 +91,7 @@ typedef enum {
   ND_BLOCK,     // { ... }
   ND_FUNCALL,   // Function call
   ND_EXPR_STMT, // Expression statement
+  ND_STMT_EXPR, // Statement expression
   ND_VAR,       // Variable
   ND_NUM,       // Integer
 } NodeKind;
@@ -112,7 +113,7 @@ struct Node {
   Node *init;
   Node *inc;
 
-  // Block
+  // Block or statement expression
   Node *body;
 
   // Function call
--- test.sh
+++ test.sh
@@ -207,4 +207,10 @@ assert 119 'int main() { return "\x77"[0]; }'
 assert 165 'int main() { return "\xA5"[0]; }'
 assert 255 'int main() { return "\x00ff"[0]; }'
 
+assert 0 'int main() { return ({ 0; }); }'
+assert 2 'int main() { return ({ 0; 1; 2; }); }'
+assert 1 'int main() { ({ 0; return 1; 2; }); return 3; }'
+assert 6 'int main() { return ({ 1; }) + ({ 2; }) + ({ 3; }); }'
+assert 3 'int main() { return ({ int x=3; x; }); }'
+
 echo OK
