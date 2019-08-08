b8a3b88dcd51d4ccf2c697641d901fa54de4aa28
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-09 07:30:00 +0900

    Support struct tags

--- test/struct.c
+++ test/struct.c
@@ -28,6 +28,11 @@ int main() {
   ASSERT(16, ({ struct {char a; int b;} x; sizeof(x); }));
   ASSERT(16, ({ struct {int a; char b;} x; sizeof(x); }));
 
+  ASSERT(16, ({ struct t {int a; int b;} x; struct t y; sizeof(y); }));
+  ASSERT(16, ({ struct t {int a; int b;}; struct t y; sizeof(y); }));
+  ASSERT(2, ({ struct t {char a[2];}; { struct t {char a[4];}; } struct t y; sizeof(y); }));
+  ASSERT(3, ({ struct t {int x;}; int t=1; struct t y; y.x=2; t+y.x; }));
+
   printf("OK\n");
   return 0;
 }
