a796af1fbe0e37bb9a4ad57044b8c8d36aa1920a
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-10 23:05:37 +0900

    Add nested type declarators

--- test/variable.c
+++ test/variable.c
@@ -53,6 +53,15 @@ int main() {
   ASSERT(8, ({ long x; sizeof(x); }));
   ASSERT(2, ({ short x; sizeof(x); }));
 
+  ASSERT(24, ({ char *x[3]; sizeof(x); }));
+  ASSERT(8, ({ char (*x)[3]; sizeof(x); }));
+  ASSERT(1, ({ char (x); sizeof(x); }));
+  ASSERT(3, ({ char (x)[3]; sizeof(x); }));
+  ASSERT(12, ({ char (x[3])[4]; sizeof(x); }));
+  ASSERT(4, ({ char (x[3])[4]; sizeof(x[0]); }));
+  ASSERT(3, ({ char *x[3]; char y; x[0]=&y; y=3; x[0][0]; }));
+  ASSERT(4, ({ char x[3]; char (*y)[3]=x; y[0][0]=4; y[0][0]; }));
+
   printf("OK\n");
   return 0;
 }
