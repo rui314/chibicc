d25364f1ee31de96e4840916ebde92e7269bef0d
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-11 16:27:43 +0900

    Add character literal

--- tokenize.c
+++ tokenize.c
@@ -219,6 +219,26 @@ static Token *read_string_literal(char *start) {
   return tok;
 }
 
+static Token *read_char_literal(char *start) {
+  char *p = start + 1;
+  if (*p == '\0')
+    error_at(start, "unclosed char literal");
+
+  char c;
+  if (*p == '\\')
+    c = read_escaped_char(&p, p + 1);
+  else
+    c = *p++;
+
+  char *end = strchr(p, '\'');
+  if (!end)
+    error_at(p, "unclosed char literal");
+
+  Token *tok = new_token(TK_NUM, start, end + 1);
+  tok->val = c;
+  return tok;
+}
+
 static void convert_keywords(Token *tok) {
   for (Token *t = tok; t->kind != TK_EOF; t = t->next)
     if (is_keyword(t))
@@ -283,6 +303,13 @@ static Token *tokenize(char *filename, char *p) {
     // String literal
     if (*p == '"') {
       cur = cur->next = read_string_literal(p);
+      p += cur->len;
+      continue;
+    }
+
+    // Character literal
+    if (*p == '\'') {
+      cur = cur->next = read_char_literal(p);
       p += cur->len;
       continue;
     }
