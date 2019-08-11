0ba3d582ad2c1a2b7e172aae79ef68b54e3b34cd
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-11 22:53:26 +0900

    Allow for-loops to define local variables

--- test/control.c
+++ test/control.c
@@ -24,6 +24,9 @@ int main() {
   ASSERT(5, ({ int i=2, j=3; (i=5,j)=6; i; }));
   ASSERT(6, ({ int i=2, j=3; (i=5,j)=6; j; }));
 
+  ASSERT(55, ({ int j=0; for (int i=0; i<=10; i=i+1) j=j+i; j; }));
+  ASSERT(3, ({ int i=3; int j=0; for (int i=0; i<=10; i=i+1) j=j+i; i; }));
+
   printf("OK\n");
   return 0;
 }
