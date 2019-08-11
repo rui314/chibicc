66fa61e90e7b7d15958e4106598c56e94bad27ef
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-11 14:12:01 +0900

    Handle complex type declarations correctly
    chibicc can now read complex type declarations such as below.
    
      long x;
      long int x;
      int long x;
      short x;
      short int x;
      int short x;
      long long x;
      long long int x;

---
+++ test/decl.c
@@ -0,0 +1,13 @@
+#include "test.h"
+
+int main() {
+  ASSERT(1, ({ char x; sizeof(x); }));
+  ASSERT(2, ({ short int x; sizeof(x); }));
+  ASSERT(2, ({ int short x; sizeof(x); }));
+  ASSERT(4, ({ int x; sizeof(x); }));
+  ASSERT(8, ({ long int x; sizeof(x); }));
+  ASSERT(8, ({ int long x; sizeof(x); }));
+
+  printf("OK\n");
+  return 0;
+}
