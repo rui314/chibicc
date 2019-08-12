bbe249eb95e379a6b4c8aad733aaec1988a683cf
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-12 10:29:17 +0900

    Add comma operator
    This patch allows writing a comma expression on the left-hand side
    of an assignment expression. This is called the "generalized lvalue"
    which is a deprecated GCC language extension. I'm implementing it
    anyway because it's useful to implement other features.

--- codegen.c
+++ codegen.c
@@ -54,6 +54,10 @@ static void gen_addr(Node *node) {
   case ND_DEREF:
     gen_expr(node->lhs);
     return;
+  case ND_COMMA:
+    gen_expr(node->lhs);
+    gen_addr(node->rhs);
+    return;
   }
 
   error_tok(node->tok, "not an lvalue");
@@ -119,6 +123,10 @@ static void gen_expr(Node *node) {
   case ND_STMT_EXPR:
     for (Node *n = node->body; n; n = n->next)
       gen_stmt(n);
+    return;
+  case ND_COMMA:
+    gen_expr(node->lhs);
+    gen_expr(node->rhs);
     return;
   case ND_FUNCALL: {
     int nargs = 0;
