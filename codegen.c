25210ccdbafb0901f5e3f347f47cf7f79d737c6c
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-13 19:41:11 +0900

    Add ~ operator

--- codegen.c
+++ codegen.c
@@ -216,6 +216,10 @@ static void gen_expr(Node *node) {
     println("  sete %%al");
     println("  movzx %%al, %%rax");
     return;
+  case ND_BITNOT:
+    gen_expr(node->lhs);
+    println("  not %%rax");
+    return;
   case ND_FUNCALL: {
     int nargs = 0;
     for (Node *arg = node->args; arg; arg = arg->next) {
--- parse.c
+++ parse.c
@@ -888,7 +888,7 @@ static Node *cast(Token **rest, Token *tok) {
   return unary(rest, tok);
 }
 
-// unary = ("+" | "-" | "*" | "&" | "!") cast
+// unary = ("+" | "-" | "*" | "&" | "!" | "~") cast
 //       | ("++" | "--") unary
 //       | postfix
 static Node *unary(Token **rest, Token *tok) {
@@ -906,6 +906,9 @@ static Node *unary(Token **rest, Token *tok) {
 
   if (equal(tok, "!"))
     return new_unary(ND_NOT, cast(rest, tok->next), tok);
+
+  if (equal(tok, "~"))
+    return new_unary(ND_BITNOT, cast(rest, tok->next), tok);
 
   // Read ++i as i+=1
   if (equal(tok, "++"))
