d06aa094530024c54141130cb63e305d885a510b
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-13 18:39:56 +0900

    Add hexadecimal, octal and binary number literals

--- chibicc.h
+++ chibicc.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 typedef struct Type Type;
 typedef struct Node Node;
--- test/literal.c
+++ test/literal.c
@@ -5,6 +5,18 @@ int main() {
   ASSERT(10, '\n');
   ASSERT(-128, '\x80');
 
+  ASSERT(511, 0777);
+  ASSERT(0, 0x0);
+  ASSERT(10, 0xa);
+  ASSERT(10, 0XA);
+  ASSERT(48879, 0xbeef);
+  ASSERT(48879, 0xBEEF);
+  ASSERT(48879, 0XBEEF);
+  ASSERT(0, 0b0);
+  ASSERT(1, 0b1);
+  ASSERT(47, 0b101111);
+  ASSERT(47, 0B101111);
+
   printf("OK\n");
   return 0;
 }
