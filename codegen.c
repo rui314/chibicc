f83b2cb7d0d3dadfe4c2693643b9feb4bf688b3a
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-15 13:48:41 +0900

    Add break statement

--- codegen.c
+++ codegen.c
@@ -358,13 +358,13 @@ static void gen_stmt(Node *node) {
     if (node->cond) {
       gen_expr(node->cond);
       println("  cmp $0, %%rax");
-      println("  je  .L.end.%d", c);
+      println("  je %s", node->brk_label);
     }
     gen_stmt(node->then);
     if (node->inc)
       gen_expr(node->inc);
     println("  jmp .L.begin.%d", c);
-    println(".L.end.%d:", c);
+    println("%s:", node->brk_label);
     return;
   }
   case ND_BLOCK:
--- parse.c
+++ parse.c
@@ -77,6 +77,9 @@ static Obj *current_fn;
 static Node *gotos;
 static Node *labels;
 
+// Current "goto" jump target.
+static char *brk_label;
+
 static bool is_typename(Token *tok);
 static Type *typespec(Token **rest, Token *tok, VarAttr *attr);
 static Type *enum_specifier(Token **rest, Token *tok);
@@ -595,6 +598,7 @@ static bool is_typename(Token *tok) {
 //      | "for" "(" expr-stmt expr? ";" expr? ")" stmt
 //      | "while" "(" expr ")" stmt
 //      | "goto" ident ";"
+//      | "break" ";"
 //      | ident ":" stmt
 //      | "{" compound-stmt
 //      | expr-stmt
@@ -627,6 +631,9 @@ static Node *stmt(Token **rest, Token *tok) {
 
     enter_scope();
 
+    char *brk = brk_label;
+    brk_label = node->brk_label = new_unique_name();
+
     if (is_typename(tok)) {
       Type *basety = typespec(&tok, tok, NULL);
       node->init = declaration(&tok, tok, basety);
@@ -643,7 +650,9 @@ static Node *stmt(Token **rest, Token *tok) {
     tok = skip(tok, ")");
 
     node->then = stmt(rest, tok);
+
     leave_scope();
+    brk_label = brk;
     return node;
   }
 
@@ -652,7 +661,11 @@ static Node *stmt(Token **rest, Token *tok) {
     tok = skip(tok->next, "(");
     node->cond = expr(&tok, tok);
     tok = skip(tok, ")");
+
+    char *brk = brk_label;
+    brk_label = node->brk_label = new_unique_name();
     node->then = stmt(rest, tok);
+    brk_label = brk;
     return node;
   }
 
@@ -662,6 +675,15 @@ static Node *stmt(Token **rest, Token *tok) {
     node->goto_next = gotos;
     gotos = node;
     *rest = skip(tok->next->next, ";");
+    return node;
+  }
+
+  if (equal(tok, "break")) {
+    if (!brk_label)
+      error_tok(tok, "stray break");
+    Node *node = new_node(ND_GOTO, tok);
+    node->unique_label = brk_label;
+    *rest = skip(tok->next, ";");
     return node;
   }
 
