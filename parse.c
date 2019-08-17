fb95a9785048f445b37129b3d5ada0abd68c7cb2
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-17 10:38:20 +0900

    Add constant expression

--- parse.c
+++ parse.c
@@ -155,6 +155,7 @@ Node *declaration();
 bool is_typename();
 Node *stmt();
 Node *expr();
+long const_expr();
 Node *assign();
 Node *conditional();
 Node *logor();
@@ -346,7 +347,7 @@ Type *abstract_declarator(Type *ty) {
   return type_suffix(ty);
 }
 
-// type-suffix = ("[" num? "]" type-suffix)?
+// type-suffix = ("[" const-expr? "]" type-suffix)?
 Type *type_suffix(Type *ty) {
   if (!consume("["))
     return ty;
@@ -354,7 +355,7 @@ Type *type_suffix(Type *ty) {
   int sz = 0;
   bool is_incomplete = true;
   if (!consume("]")) {
-    sz = expect_number();
+    sz = const_expr();
     is_incomplete = false;
     expect("]");
   }
@@ -453,7 +454,8 @@ Type *struct_decl() {
 // enum-specifier = "enum" ident
 //                | "enum" ident? "{" enum-list? "}"
 //
-// enum-list = ident ("=" num)? ("," ident ("=" num)?)* ","?
+// enum-list = enum-elem ("," enum-elem)* ","?
+// enum-elem = ident ("=" const-expr)?
 Type *enum_specifier() {
   expect("enum");
   Type *ty = enum_type();
@@ -476,7 +478,7 @@ Type *enum_specifier() {
   for (;;) {
     char *name = expect_ident();
     if (consume("="))
-      cnt = expect_number();
+      cnt = const_expr();
 
     VarScope *sc = push_scope(name);
     sc->enum_ty = ty;
@@ -656,7 +658,7 @@ bool is_typename() {
 // stmt = "return" expr ";"
 //      | "if" "(" expr ")" stmt ("else" stmt)?
 //      | "switch" "(" expr ")" stmt
-//      | "case" num ":" stmt
+//      | "case" const-expr ":" stmt
 //      | "default" ":" stmt
 //      | "while" "(" expr ")" stmt
 //      | "for" "(" (expr? ";" | declaration) expr? ";" expr? ")" stmt
@@ -702,7 +704,7 @@ Node *stmt() {
   if (tok = consume("case")) {
     if (!current_switch)
       error_tok(tok, "stray case");
-    int val = expect_number();
+    int val = const_expr();
     expect(":");
 
     Node *node = new_unary(ND_CASE, stmt(), tok);
@@ -818,6 +820,57 @@ Node *expr() {
     node = new_binary(ND_COMMA, node, assign(), tok);
   }
   return node;
+}
+
+long eval(Node *node) {
+  switch (node->kind) {
+  case ND_ADD:
+    return eval(node->lhs) + eval(node->rhs);
+  case ND_SUB:
+    return eval(node->lhs) - eval(node->rhs);
+  case ND_MUL:
+    return eval(node->lhs) * eval(node->rhs);
+  case ND_DIV:
+    return eval(node->lhs) / eval(node->rhs);
+  case ND_BITAND:
+    return eval(node->lhs) & eval(node->rhs);
+  case ND_BITOR:
+    return eval(node->lhs) | eval(node->rhs);
+  case ND_BITXOR:
+    return eval(node->lhs) | eval(node->rhs);
+  case ND_SHL:
+    return eval(node->lhs) << eval(node->rhs);
+  case ND_SHR:
+    return eval(node->lhs) >> eval(node->rhs);
+  case ND_EQ:
+    return eval(node->lhs) == eval(node->rhs);
+  case ND_NE:
+    return eval(node->lhs) != eval(node->rhs);
+  case ND_LT:
+    return eval(node->lhs) < eval(node->rhs);
+  case ND_LE:
+    return eval(node->lhs) <= eval(node->rhs);
+  case ND_TERNARY:
+    return eval(node->cond) ? eval(node->then) : eval(node->els);
+  case ND_COMMA:
+    return eval(node->rhs);
+  case ND_NOT:
+    return !eval(node->lhs);
+  case ND_BITNOT:
+    return ~eval(node->lhs);
+  case ND_LOGAND:
+    return eval(node->lhs) && eval(node->rhs);
+  case ND_LOGOR:
+    return eval(node->lhs) || eval(node->rhs);
+  case ND_NUM:
+    return node->val;
+  }
+
+  error_tok(node->tok, "not a constant expression");
+}
+
+long const_expr() {
+  return eval(conditional());
 }
 
 // assign    = conditional (assign-op assign)?
