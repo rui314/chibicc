5b0e79f99af4423a4e28f8e180ac49471b172afe
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-18 19:12:02 +0900

    Add scalar global initializer

--- codegen.c
+++ codegen.c
@@ -519,13 +519,19 @@ static void emit_data(Program *prog) {
     Var *var = vl->var;
     printf("%s:\n", var->name);
 
-    if (!var->contents) {
+    if (!var->initializer) {
       printf("  .zero %d\n", var->ty->size);
       continue;
     }
 
-    for (int i = 0; i < var->cont_len; i++)
-      printf("  .byte %d\n", var->contents[i]);
+    for (Initializer *init = var->initializer; init; init = init->next) {
+      if (init->label)
+        printf("  .quad %s\n", init->label);
+      else if (init->sz == 1)
+        printf("  .byte %ld\n", init->val);
+      else
+        printf("  .%dbyte %ld\n", init->sz, init->val);
+    }
   }
 }
 
--- parse.c
+++ parse.c
@@ -185,6 +185,7 @@ static bool is_typename(void);
 static Node *stmt(void);
 static Node *stmt2(void);
 static Node *expr(void);
+static long eval(Node *node);
 static long const_expr(void);
 static Node *assign(void);
 static Node *conditional(void);
@@ -658,6 +659,53 @@ static Function *function(void) {
 }
 
 // global-var = basetype declarator type-suffix ";"
+static Initializer *new_init_val(Initializer *cur, int sz, int val) {
+  Initializer *init = calloc(1, sizeof(Initializer));
+  init->sz = sz;
+  init->val = val;
+  cur->next = init;
+  return init;
+}
+
+static Initializer *new_init_label(Initializer *cur, char *label) {
+  Initializer *init = calloc(1, sizeof(Initializer));
+  init->label = label;
+  cur->next = init;
+  return init;
+}
+
+static Initializer *gvar_init_string(char *p, int len) {
+  Initializer head = {};
+  Initializer *cur = &head;
+  for (int i = 0; i < len; i++)
+    cur = new_init_val(cur, 1, p[i]);
+  return head.next;
+}
+
+// gvar-initializer2 = assign
+static Initializer *gvar_initializer2(Initializer *cur, Type *ty) {
+  Token *tok = token;
+  Node *expr = conditional();
+
+  if (expr->kind == ND_ADDR) {
+    if (expr->lhs->kind != ND_VAR)
+      error_tok(tok, "invalid initializer");
+    return new_init_label(cur, expr->lhs->var->name);
+  }
+
+  if (expr->kind == ND_VAR && expr->var->ty->kind == TY_ARRAY)
+    return new_init_label(cur, expr->var->name);
+
+  return new_init_val(cur, ty->size, eval(expr));
+}
+
+static Initializer *gvar_initializer(Type *ty) {
+  Initializer head = {};
+  gvar_initializer2(&head, ty);
+  return head.next;
+}
+
+// global-var = basetype declarator type-suffix ("=" gvar-initializer)? ";"
 static void global_var(void) {
   StorageClass sclass;
   Type *ty = basetype(&sclass);
@@ -665,18 +713,24 @@ static void global_var(void) {
   Token *tok = token;
   ty = declarator(ty, &name);
   ty = type_suffix(ty);
-  expect(";");
-
-  if (ty->is_incomplete)
-    error_tok(tok, "incomplete type");
 
   if (sclass == TYPEDEF) {
+    expect(";");
     push_scope(name)->type_def = ty;
-  } else {
+    return;
+  }
+
+  Var *var = new_gvar(name, ty, true);
+
+  if (!consume("=")) {
     if (ty->is_incomplete)
       error_tok(tok, "incomplete type");
-    new_gvar(name, ty, true);
+    expect(";");
+    return;
   }
+
+  var->initializer = gvar_initializer(ty);
+  expect(";");
 }
 
 typedef struct Designator Designator;
@@ -1539,8 +1593,7 @@ static Node *primary(void) {
 
     Type *ty = array_of(char_type, tok->cont_len);
     Var *var = new_gvar(new_label(), ty, true);
-    var->contents = tok->contents;
-    var->cont_len = tok->cont_len;
+    var->initializer = gvar_init_string(tok->contents, tok->cont_len);
     return new_var_node(var, tok);
   }
 
