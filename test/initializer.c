d88f3f1adea1430139c03aa7db9bf27f02bda847
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-19 06:53:50 +0900

    Allow parentheses in initializers to be omitted

--- test/initializer.c
+++ test/initializer.c
@@ -7,7 +7,7 @@ long g6 = 6;
 int g9[3] = {0, 1, 2};
 struct {char a; int b;} g11[2] = {{1, 2}, {3, 4}};
 struct {int a[2];} g12[2] = {{{1, 2}}};
-union { int a; char b[8]; } g13[2] = {{0x01020304}, {0x05060708}};
+union { int a; char b[8]; } g13[2] = {0x01020304, 0x05060708};
 char g17[] = "foobar";
 char g18[10] = "foobar";
 char g19[3] = "foobar";
@@ -23,6 +23,9 @@ int *g28 = &g11[1].a;
 long g29 = (long)(long)g26;
 struct { struct { int a[3]; } a; } g30 = {{{1,2,3}}};
 int *g31=g30.a.a;
+struct {int a[2];} g40[2] = {{1, 2}, 3, 4};
+struct {int a[2];} g41[2] = {1, 2, 3, 4};
+char g43[][4] = {'f', 'o', 'o', 0, 'b', 'a', 'r', 0};
 
 int main() {
   ASSERT(1, ({ int x[3]={1,2,3}; x[0]; }));
@@ -135,6 +138,25 @@ int main() {
   ASSERT(1, g31[0]);
   ASSERT(2, g31[1]);
   ASSERT(3, g31[2]);
+
+  ASSERT(1, g40[0].a[0]);
+  ASSERT(2, g40[0].a[1]);
+  ASSERT(3, g40[1].a[0]);
+  ASSERT(4, g40[1].a[1]);
+
+  ASSERT(1, g41[0].a[0]);
+  ASSERT(2, g41[0].a[1]);
+  ASSERT(3, g41[1].a[0]);
+  ASSERT(4, g41[1].a[1]);
+
+  ASSERT(0, ({ int x[2][3]={0,1,2,3,4,5}; x[0][0]; }));
+  ASSERT(3, ({ int x[2][3]={0,1,2,3,4,5}; x[1][0]; }));
+
+  ASSERT(0, ({ struct {int a; int b;} x[2]={0,1,2,3}; x[0].a; }));
+  ASSERT(2, ({ struct {int a; int b;} x[2]={0,1,2,3}; x[1].a; }));
+
+  ASSERT(0, strcmp(g43[0], "foo"));
+  ASSERT(0, strcmp(g43[1], "bar"));
 
   printf("OK\n");
   return 0;
