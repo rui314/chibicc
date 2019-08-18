7a3ba2aac1d8f8cef5215acab742820e68efe601
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-18 20:35:17 +0900

    Add struct initializer for global variable

--- test/initializer.c
+++ test/initializer.c
@@ -4,6 +4,9 @@ char g3 = 3;
 short g4 = 4;
 int g5 = 5;
 long g6 = 6;
+int g9[3] = {0, 1, 2};
+struct {char a; int b;} g11[2] = {{1, 2}, {3, 4}};
+struct {int a[2];} g12[2] = {{{1, 2}}};
 
 int main() {
   ASSERT(1, ({ int x[3]={1,2,3}; x[0]; }));
@@ -71,6 +74,20 @@ int main() {
   ASSERT(4, g4);
   ASSERT(5, g5);
   ASSERT(6, g6);
+
+  ASSERT(0, g9[0]);
+  ASSERT(1, g9[1]);
+  ASSERT(2, g9[2]);
+
+  ASSERT(1, g11[0].a);
+  ASSERT(2, g11[0].b);
+  ASSERT(3, g11[1].a);
+  ASSERT(4, g11[1].b);
+
+  ASSERT(1, g12[0].a[0]);
+  ASSERT(2, g12[0].a[1]);
+  ASSERT(0, g12[1].a[0]);
+  ASSERT(0, g12[1].a[1]);
 
   printf("OK\n");
   return 0;
