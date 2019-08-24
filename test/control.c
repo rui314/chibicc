ee252e6ce79d752526504cf034fd41f070191824
Rui Ueyama <ruiu@cs.stanford.edu>
2019-08-24 16:09:46 +0900

    Add do ... while

--- test/control.c
+++ test/control.c
@@ -65,6 +65,9 @@ int main() {
 
   ASSERT(3, ({ int i=0; switch(-1) { case 0xffffffff: i=3; break; } i; }));
 
+  ASSERT(7, ({ int i=0; int j=0; do { j++; } while (i++ < 6); j; }));
+  ASSERT(4, ({ int i=0; int j=0; int k=0; do { if (++j > 3) break; continue; k++; } while (1); j; }));
+
   printf("OK\n");
   return 0;
 }
