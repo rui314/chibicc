b9ad3e43cf7479712972514aa3f2c55a0f650f76
Rui Ueyama <ruiu@cs.stanford.edu>
2020-03-30 10:37:44 +0900

    Add multi-arity funclike #define

--- test/macro.c
+++ test/macro.c
@@ -204,6 +204,15 @@ int main() {
 #define M7 ()
   assert(3, ret3 M7, "ret3 M7");
 
+#define M8(x,y) x+y
+  assert(7, M8(3, 4), "M8(3, 4)");
+
+#define M8(x,y) x*y
+  assert(24, M8(3+4, 4+5), "M8(3+4, 4+5)");
+
+#define M8(x,y) (x)*(y)
+  assert(63, M8(3+4, 4+5), "M8(3+4, 4+5)");
+
   printf("OK\n");
   return 0;
 }
