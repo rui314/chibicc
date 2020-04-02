ca52adc8dfd0659b711e37046c50f6a17b65febe
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-02 21:54:47 +0900

    Add union

--- chibicc.h
+++ chibicc.h
@@ -153,6 +153,7 @@ typedef enum {
   TY_FUNC,
   TY_ARRAY,
   TY_STRUCT,
+  TY_UNION,
 } TypeKind;
 
 struct Type {
--- tokenize.c
+++ tokenize.c
@@ -126,7 +126,7 @@ static int read_punct(char *p) {
 static bool is_keyword(Token *tok) {
   static char *kw[] = {
     "return", "if", "else", "for", "while", "int", "sizeof", "char",
-    "struct",
+    "struct", "union",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
