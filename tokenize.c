198f0e8f1db314eb341b4299810d3aaa08b9955b
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-02 21:54:47 +0900

    Add union

--- tokenize.c
+++ tokenize.c
@@ -117,7 +117,7 @@ static int from_hex(char c) {
 static bool is_keyword(Token *tok) {
   static char *kw[] = {
     "return", "if", "else", "for", "while", "int", "sizeof", "char",
-    "struct",
+    "struct", "union",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
