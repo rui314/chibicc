5161e203fdb32639915271974a4d370704a1b87d
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-18 01:05:18 +0900

    Add struct assignment

--- test/union.c
+++ test/union.c
@@ -7,6 +7,9 @@ int main() {
   ASSERT(0, ({ union { int a; char b[4]; } x; x.a = 515; x.b[2]; }));
   ASSERT(0, ({ union { int a; char b[4]; } x; x.a = 515; x.b[3]; }));
 
+  ASSERT(3, ({ union {int a,b;} x,y; x.a=3; y.a=5; y=x; y.a; }));
+  ASSERT(3, ({ union {struct {int a,b;} c;} x,y; x.c.b=3; y.c.b=5; y=x; y.c.b; }));
+
   printf("OK\n");
   return 0;
 }
