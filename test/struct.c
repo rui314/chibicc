d33cb89956326ff7b6bf10d3c0e45416ea52043b
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-18 01:05:18 +0900

    Add struct assignment

--- test/struct.c
+++ test/struct.c
@@ -36,6 +36,16 @@ int main() {
   ASSERT(3, ({ struct t {char a;} x; struct t *y = &x; x.a=3; y->a; }));
   ASSERT(3, ({ struct t {char a;} x; struct t *y = &x; y->a=3; x.a; }));
 
+  ASSERT(3, ({ struct {int a,b;} x,y; x.a=3; y=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y; struct t *z=&y; *z=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y, *p=&x, *q=&y; *q=*p; y.a; }));
+  ASSERT(5, ({ struct t {char a, b;} x, y; x.a=5; y=x; y.a; }));
+
+  ASSERT(3, ({ struct {int a,b;} x,y; x.a=3; y=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y; struct t *z=&y; *z=x; y.a; }));
+  ASSERT(7, ({ struct t {int a,b;}; struct t x; x.a=7; struct t y, *p=&x, *q=&y; *q=*p; y.a; }));
+  ASSERT(5, ({ struct t {char a, b;} x, y; x.a=5; y=x; y.a; }));
+
   printf("OK\n");
   return 0;
 }
