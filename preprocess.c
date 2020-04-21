9e780cce10ae24f3883aa0b166a73896deab7fc3
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-21 10:46:26 +0900

    Skip extra tokens after `#include "..."`

--- preprocess.c
+++ preprocess.c
@@ -4,6 +4,17 @@ static bool is_hash(Token *tok) {
   return tok->at_bol && equal(tok, "#");
 }
 
+// Some preprocessor directives such as #include allow extraneous
+// tokens before newline. This function skips such tokens.
+static Token *skip_line(Token *tok) {
+  if (tok->at_bol)
+    return tok;
+  warn_tok(tok, "extra token");
+  while (tok->at_bol)
+    tok = tok->next;
+  return tok;
+}
+
 static Token *copy_token(Token *tok) {
   Token *t = calloc(1, sizeof(Token));
   *t = *tok;
@@ -51,7 +62,8 @@ static Token *preprocess2(Token *tok) {
       Token *tok2 = tokenize_file(path);
       if (!tok2)
         error_tok(tok, "%s", strerror(errno));
-      tok = append(tok2, tok->next);
+      tok = skip_line(tok->next);
+      tok = append(tok2, tok);
       continue;
     }
 
