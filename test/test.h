2d737a1d7f23131130cd35fa812d0132e9b48282
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-26 10:11:02 +0900

    Concatenate adjacent string literals

--- test/test.h
+++ test/test.h
@@ -4,5 +4,6 @@ void assert(int expected, int actual, char *code);
 int printf(char *fmt, ...);
 int sprintf(char *buf, char *fmt, ...);
 int strcmp(char *p, char *q);
+int strncmp(char *p, char *q, long n);
 int memcmp(char *p, char *q, long n);
 void exit(int n);
