cf055ce460515646f158c3faf4edcd38b71e0179
Rui Ueyama <ruiu@cs.stanford.edu>
2020-04-30 23:33:27 +0900

    Implement default argument promotion for float

--- parse.c
+++ parse.c
@@ -2158,6 +2158,10 @@ static Node *funcall(Token **rest, Token *tok) {
         error_tok(arg->tok, "passing struct or union is not supported yet");
       arg = new_cast(arg, param_ty);
       param_ty = param_ty->next;
+    } else if (arg->ty->kind == TY_FLOAT) {
+      // If parameter type is omitted (e.g. in "..."), float
+      // arguments are promoted to double.
+      arg = new_cast(arg, ty_double);
     }
 
     cur = cur->next = arg;
