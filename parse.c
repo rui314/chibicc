4e4a68790291f39e748fbcf32334d09f91b21f48
Rui Ueyama <ruiu@cs.stanford.edu>
2020-05-05 16:01:55 +0900

    Allow extraneous comma at the end of enum or initializer list

--- parse.c
+++ parse.c
@@ -556,10 +556,28 @@ static Type *typename(Token **rest, Token *tok) {
   return abstract_declarator(rest, tok, ty);
 }
 
+static bool is_end(Token *tok) {
+  return equal(tok, "}") || (equal(tok, ",") && equal(tok->next, "}"));
+}
+
+static bool consume_end(Token **rest, Token *tok) {
+  if (equal(tok, "}")) {
+    *rest = tok->next;
+    return true;
+  }
+
+  if (equal(tok, ",") && equal(tok->next, "}")) {
+    *rest = tok->next->next;
+    return true;
+  }
+
+  return false;
+}
+
 // enum-specifier = ident? "{" enum-list? "}"
 //                | ident ("{" enum-list? "}")?
 //
-// enum-list      = ident ("=" num)? ("," ident ("=" num)?)*
+// enum-list      = ident ("=" num)? ("," ident ("=" num)?)* ","?
 static Type *enum_specifier(Token **rest, Token *tok) {
   Type *ty = enum_type();
 
@@ -585,7 +603,7 @@ static Type *enum_specifier(Token **rest, Token *tok) {
   // Read an enum-list.
   int i = 0;
   int val = 0;
-  while (!equal(tok, "}")) {
+  while (!consume_end(rest, tok)) {
     if (i++ > 0)
       tok = skip(tok, ",");
 
@@ -599,8 +617,6 @@ static Type *enum_specifier(Token **rest, Token *tok) {
     sc->enum_ty = ty;
     sc->enum_val = val++;
   }
-
-  *rest = tok->next;
 
   if (tag)
     push_tag_scope(tag, ty);
@@ -664,7 +680,7 @@ static int count_array_init_elements(Token *tok, Type *ty) {
   Initializer *dummy = new_initializer(ty->base, false);
   int i = 0;
 
-  for (; !equal(tok, "}"); i++) {
+  for (; !consume_end(&tok, tok); i++) {
     if (i > 0)
       tok = skip(tok, ",");
     initializer2(&tok, tok, dummy);
@@ -672,7 +688,7 @@ static int count_array_init_elements(Token *tok, Type *ty) {
   return i;
 }
 
-// array-initializer1 = "{" initializer ("," initializer)* "}"
+// array-initializer1 = "{" initializer ("," initializer)* ","? "}"
 static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
   tok = skip(tok, "{");
 
@@ -681,7 +697,7 @@ static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
     *init = *new_initializer(array_of(init->ty->base, len), false);
   }
 
-  for (int i = 0; !consume(rest, tok, "}"); i++) {
+  for (int i = 0; !consume_end(rest, tok); i++) {
     if (i > 0)
       tok = skip(tok, ",");
 
@@ -699,7 +715,7 @@ static void array_initializer2(Token **rest, Token *tok, Initializer *init) {
     *init = *new_initializer(array_of(init->ty->base, len), false);
   }
 
-  for (int i = 0; i < init->ty->array_len && !equal(tok, "}"); i++) {
+  for (int i = 0; i < init->ty->array_len && !is_end(tok); i++) {
     if (i > 0)
       tok = skip(tok, ",");
     initializer2(&tok, tok, init->children[i]);
@@ -707,13 +723,13 @@ static void array_initializer2(Token **rest, Token *tok, Initializer *init) {
   *rest = tok;
 }
 
-// struct-initializer1 = "{" initializer ("," initializer)* "}"
+// struct-initializer1 = "{" initializer ("," initializer)* ","? "}"
 static void struct_initializer1(Token **rest, Token *tok, Initializer *init) {
   tok = skip(tok, "{");
 
   Member *mem = init->ty->members;
 
-  while (!consume(rest, tok, "}")) {
+  while (!consume_end(rest, tok)) {
     if (mem != init->ty->members)
       tok = skip(tok, ",");
 
@@ -728,7 +744,7 @@ static void struct_initializer1(Token **rest, Token *tok, Initializer *init) {
 
 // struct-initializer2 = initializer ("," initializer)*
 static void struct_initializer2(Token **rest, Token *tok, Initializer *init) {
-  for (Member *mem = init->ty->members; mem && !equal(tok, "}"); mem = mem->next) {
+  for (Member *mem = init->ty->members; mem && !is_end(tok); mem = mem->next) {
     if (mem != init->ty->members)
       tok = skip(tok, ",");
     initializer2(&tok, tok, init->children[mem->idx]);
