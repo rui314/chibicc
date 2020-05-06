7305bd02a4eaa1bfcb6bd279fb8a55056cbcb947
Rui Ueyama <ruiu@cs.stanford.edu>
2020-05-06 18:47:42 +0900

    Accept multibyte character as wide character literal
    On most Unix-like systems, wide character literal is 32-bit long
    and encodes a Unicode code point. On Windows, that is 16-bit
    long and encodes a UTF-16 code unit. Clearly, there's a portability
    issue here. Personally I've never used wide characters in my code
    as I didn't find it useful.
    
    Being said that, some header files contain wide character literal,
    so we need to support that so that chibicc can include such files.
    
    We assume that source files are always encoded in UTF-8.

--- unicode.c
+++ unicode.c
@@ -26,3 +26,43 @@ int encode_utf8(char *buf, uint32_t c) {
   buf[3] = 0b10000000 | (c & 0b00111111);
   return 4;
 }
+
+// Read a UTF-8-encoded Unicode code point from a source file.
+// We assume that source files are always in UTF-8.
+//
+// UTF-8 is a variable-width encoding in which one code point is
+// encoded in one to four bytes. One byte UTF-8 code points are
+// identical to ASCII. Non-ASCII characters are encoded using more
+// than one byte.
+uint32_t decode_utf8(char **new_pos, char *p) {
+  if ((unsigned char)*p < 128) {
+    *new_pos = p + 1;
+    return *p;
+  }
+
+  char *start = p;
+  int len;
+  uint32_t c;
+
+  if ((unsigned char)*p >= 0b11110000) {
+    len = 4;
+    c = *p & 0b111;
+  } else if ((unsigned char)*p >= 0b11100000) {
+    len = 3;
+    c = *p & 0b1111;
+  } else if ((unsigned char)*p >= 0b11000000) {
+    len = 2;
+    c = *p & 0b11111;
+  } else {
+    error_at(start, "invalid UTF-8 sequence");
+  }
+
+  for (int i = 1; i < len; i++) {
+    if ((unsigned char)p[i] >> 6 != 0b10)
+      error_at(start, "invalid UTF-8 sequence");
+    c = (c << 6) | (p[i] & 0b111111);
+  }
+
+  *new_pos = p + len;
+  return c;
+}
