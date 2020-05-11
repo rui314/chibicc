ea39a23e6360ea1f0d4ede0cdbd6ef391d177933
Rui Ueyama <ruiu@cs.stanford.edu>
2020-05-11 20:21:17 +0900

    Make "main" to implicitly return 0

--- codegen.c
+++ codegen.c
@@ -1283,6 +1283,13 @@ static void emit_text(Obj *prog) {
     gen_stmt(fn->body);
     assert(depth == 0);
 
+    // [https://www.sigbus.info/n1570#5.1.2.2.3p1] The C spec defines
+    // a special rule for the main function. Reaching the end of the
+    // main function is equivalent to returning 0, even though the
+    // behavior is undefined for the other functions.
+    if (strcmp(fn->name, "main") == 0)
+      println("  mov $0, %%rax");
+
     // Epilogue
     println(".L.return.%s:", fn->name);
     println("  mov %%rbp, %%rsp");
