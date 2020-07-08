9912d0f0aa62a78b317e7874ece108416e529f05
Rui Ueyama <ruiu@cs.stanford.edu>
2020-07-08 13:01:34 +0900

    Define __STDC_UTF_{16,32}__ macros
    These predefined macros indicates that our u and U chars/strings
    are UTF-16 and UTF-32-encoded, respectively.

--- preprocess.c
+++ preprocess.c
@@ -911,6 +911,8 @@ void init_macros(void) {
   define_macro("__STDC_NO_COMPLEX__", "1");
   define_macro("__STDC_NO_THREADS__", "1");
   define_macro("__STDC_NO_VLA__", "1");
+  define_macro("__STDC_UTF_16__", "1");
+  define_macro("__STDC_UTF_32__", "1");
   define_macro("__STDC_VERSION__", "201112L");
   define_macro("__STDC__", "1");
   define_macro("__USER_LABEL_PREFIX__", "");
