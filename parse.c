483b194a80e904c11c5c6d855303596145adacee
Rui Ueyama <ruiu@cs.stanford.edu>
2020-07-20 23:23:27 +0900

    Handle union initializers for local variables

--- parse.c
+++ parse.c
@@ -244,7 +244,7 @@ static Initializer *new_initializer(Type *ty, bool is_flexible) {
     return init;
   }
 
-  if (ty->kind == TY_STRUCT) {
+  if (ty->kind == TY_STRUCT || ty->kind == TY_UNION) {
     // Count the number of struct members.
     int len = 0;
     for (Member *mem = ty->members; mem; mem = mem->next)
@@ -701,8 +701,17 @@ static void struct_initializer(Token **rest, Token *tok, Initializer *init) {
   }
 }
 
+static void union_initializer(Token **rest, Token *tok, Initializer *init) {
+  // Unlike structs, union initializers take only one initializer,
+  // and that initializes the first union member.
+  tok = skip(tok, "{");
+  initializer2(&tok, tok, init->children[0]);
+  *rest = skip(tok, "}");
+}
+
 // initializer = string-initializer | array-initializer
-//             | struct-initializer | assign
+//             | struct-initializer | union-initializer
+//             | assign
 static void initializer2(Token **rest, Token *tok, Initializer *init) {
   if (init->ty->kind == TY_ARRAY && tok->kind == TK_STR) {
     string_initializer(rest, tok, init);
@@ -728,6 +737,11 @@ static void initializer2(Token **rest, Token *tok, Initializer *init) {
     }
 
     struct_initializer(rest, tok, init);
+    return;
+  }
+
+  if (init->ty->kind == TY_UNION) {
+    union_initializer(rest, tok, init);
     return;
   }
 
@@ -776,6 +790,11 @@ static Node *create_lvar_init(Initializer *init, Type *ty, InitDesg *desg, Token
       node = new_binary(ND_COMMA, node, rhs, tok);
     }
     return node;
+  }
+
+  if (ty->kind == TY_UNION) {
+    InitDesg desg2 = {desg, 0, ty->members};
+    return create_lvar_init(init->children[0], ty->members->ty, &desg2, tok);
   }
 
   if (!init->expr)
