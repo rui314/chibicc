28882cc288061e62e905d0867fe27563575e9c17
Rui Ueyama <ruiu@cs.stanford.edu>
2020-07-20 23:53:12 +0900

    Handle union initializers for global variable

--- chibicc.h
+++ chibicc.h
@@ -16,6 +16,7 @@
 typedef struct Type Type;
 typedef struct Node Node;
 typedef struct Member Member;
+typedef struct Relocation Relocation;
 
 //
 // tokenize.c
@@ -77,12 +78,24 @@ struct Obj {
 
   // Global variable
   char *init_data;
+  Relocation *rel;
 
   // Function
   Obj *params;
   Node *body;
   Obj *locals;
   int stack_size;
+};
+
+// Global variable can be initialized either by a constant expression
+// or a pointer to another global variable. This struct represents the
+// latter.
+typedef struct Relocation Relocation;
+struct Relocation {
+  Relocation *next;
+  int offset;
+  char *label;
+  long addend;
 };
 
 // AST node
