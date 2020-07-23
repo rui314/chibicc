338144869fa82097d7767a032cbaac616ba0cd01
Rui Ueyama <ruiu@cs.stanford.edu>
2020-07-23 13:25:15 +0900

    Add __VA_OPT__

--- test/macro.c
+++ test/macro.c
@@ -377,6 +377,11 @@ int main() {
 
   ASSERT(0, strcmp(__BASE_FILE__, "test/macro.c"));
 
+#define M30(buf, fmt, ...) sprintf(buf, fmt __VA_OPT__(,) __VA_ARGS__)
+  ASSERT(0, ({ char buf[100]; M30(buf, "foo"); strcmp(buf, "foo"); }));
+  ASSERT(0, ({ char buf[100]; M30(buf, "foo%d", 3); strcmp(buf, "foo3"); }));
+  ASSERT(0, ({ char buf[100]; M30(buf, "foo%d%d", 3, 5); strcmp(buf, "foo35"); }));
+
   printf("OK\n");
   return 0;
 }
