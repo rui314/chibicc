ec844b9e7d14c0da97c6031e3ff2eb26cff224d6
Rui Ueyama <ruiu@cs.stanford.edu>
2020-07-24 16:18:03 +0900

    Add _Generic

---
+++ test/generic.c
@@ -0,0 +1,12 @@
+#include "test.h"
+
+int main() {
+  ASSERT(1, _Generic(100.0, double: 1, int *: 2, int: 3, float: 4));
+  ASSERT(2, _Generic((int *)0, double: 1, int *: 2, int: 3, float: 4));
+  ASSERT(2, _Generic((int[3]){}, double: 1, int *: 2, int: 3, float: 4));
+  ASSERT(3, _Generic(100, double: 1, int *: 2, int: 3, float: 4));
+  ASSERT(4, _Generic(100f, double: 1, int *: 2, int: 3, float: 4));
+
+  printf("OK\n");
+  return 0;
+}
