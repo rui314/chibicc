31dc1dfa211ee27e74907ce3aa3986401dcedb82
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-15 17:01:54 +0900

    Add union designated initializer

--- test/initializer.c
+++ test/initializer.c
@@ -27,6 +27,8 @@ struct {int a[2];} g40[2] = {{1, 2}, 3, 4};
 struct {int a[2];} g41[2] = {1, 2, 3, 4};
 char g43[][4] = {'f', 'o', 'o', 0, 'b', 'a', 'r', 0};
 char *g44 = {"foo"};
+union { int a; char b[4]; } g50 = {.b[2]=0x12};
+union { int a; } g51[2] = {};
 
 typedef char T60[];
 T60 g60 = {1, 2, 3};
@@ -245,6 +247,13 @@ int main() {
 
   ASSERT(5, ((struct { int a,b,c; }){ .c=5 }).c);
   ASSERT(0, ((struct { int a,b,c; }){ .c=5 }).a);
+
+  ASSERT(0x00ff, ({ union { unsigned short a; char b[2]; } x={.b[0]=0xff}; x.a; }));
+  ASSERT(0xff00, ({ union { unsigned short a; char b[2]; } x={.b[1]=0xff}; x.a; }));
+
+  ASSERT(0x00120000, g50.a);
+  ASSERT(0, g51[0].a);
+  ASSERT(0, g51[1].a);
 
   printf("OK\n");
   return 0;
