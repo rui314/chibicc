1ac475c4278d32d644d8210f12f2e7f066e30c33
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-18 11:46:32 +0900

    Ignore -O, -W and -g and other flags

--- test/driver.sh
+++ test/driver.sh
@@ -103,4 +103,10 @@ check -D
 echo foo | $chibicc -Dfoo=bar -Ufoo -E - | grep -q foo
 check -U
 
+# ignored options
+$chibicc -c -O -Wall -g -std=c11 -ffreestanding -fno-builtin \
+         -fno-omit-frame-pointer -fno-stack-protector -fno-strict-aliasing \
+         -m64 -mno-red-zone -w -o /dev/null $tmp/empty.c
+check 'ignored options'
+
 echo OK
