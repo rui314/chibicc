45d81976875c827882c7deda8731e5fbfd245823
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-18 11:56:24 +0900

    Add \u and \U escape sequences

---
+++ test/unicode.c
@@ -0,0 +1,14 @@
+#include "test.h"
+
+int main() {
+  ASSERT(4, sizeof(L'\0'));
+  ASSERT(97, L'a');
+
+  ASSERT(0, strcmp("αβγ", "\u03B1\u03B2\u03B3"));
+  ASSERT(0, strcmp("日本語", "\u65E5\u672C\u8A9E"));
+  ASSERT(0, strcmp("日本語", "\U000065E5\U0000672C\U00008A9E"));
+  ASSERT(0, strcmp("🌮", "\U0001F32E"));
+
+  printf("OK\n");
+  return 0;
+}
