f401374a857414dabcbf7536375bf8e7e353adf7
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-18 12:42:46 +0900

    Add -MD option

--- test/driver.sh
+++ test/driver.sh
@@ -242,4 +242,17 @@ check -MT
 $chibicc -MT foo -MT bar -M -I$tmp $tmp/out.c | grep -q '^foo bar:'
 check -MT
 
+# -MD
+echo '#include "out2.h"' > $tmp/md2.c
+echo '#include "out3.h"' > $tmp/md3.c
+(cd $tmp; $OLDPWD/$chibicc -c -MD -I. md2.c md3.c)
+grep -q -z '^md2.o:.* md2\.c .* ./out2\.h' $tmp/md2.d
+check -MD
+grep -q -z '^md3.o:.* md3\.c .* ./out3\.h' $tmp/md3.d
+check -MD
+
+$chibicc -c -MD -MF $tmp/md-mf.d -I. $tmp/md2.c
+grep -q -z '^md2.o:.*md2\.c .*/out2\.h' $tmp/md-mf.d
+check -MD
+
 echo OK
