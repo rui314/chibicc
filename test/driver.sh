db850f37a2a284bf18cea427e4676a22d83d04b8
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-18 12:38:21 +0900

    Add -MT option

--- test/driver.sh
+++ test/driver.sh
@@ -240,4 +240,10 @@ check -MP
 grep -q '^.*/out3.h:' $tmp/mp
 check -MP
 
+# -MT
+$chibicc -MT foo -M -I$tmp $tmp/out.c | grep -q '^foo:'
+check -MT
+$chibicc -MT foo -MT bar -M -I$tmp $tmp/out.c | grep -q '^foo bar:'
+check -MT
+
 echo OK
