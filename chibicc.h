592f89f3c2a1d2935ad49f8c8ed4a1d08a6f51fb
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-20 19:36:36 +0900

    Add #if and #endif

--- chibicc.h
+++ chibicc.h
@@ -229,6 +229,7 @@ struct Node {
 };
 
 Node *new_cast(Node *expr, Type *ty);
+int64_t const_expr(Token **rest, Token *tok);
 Var *parse(Token *tok);
 
 //
--- preprocess.c
+++ preprocess.c
@@ -1,5 +1,14 @@
 #include "chibicc.h"
 
+// `#if` can be nested, so we use a stack to manage nested `#if`s.
+typedef struct CondIncl CondIncl;
+struct CondIncl {
+  CondIncl *next;
+  Token *tok;
+};
+
+static CondIncl *cond_incl;
+
 static char *format(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -11,7 +20,6 @@ static char *format(char *fmt, ...) {
   fclose(out);
   return buf;
 }
-
 static bool is_hash(Token *tok) {
   return tok->at_bol && equal(tok, "#");
 }
@@ -34,6 +42,13 @@ static Token *copy_token(Token *tok) {
   return t;
 }
 
+static Token *new_eof(Token *tok) {
+  Token *t = copy_token(tok);
+  t->kind = TK_EOF;
+  t->len = 0;
+  return t;
+}
+
 // Append tok2 to the end of tok1.
 static Token *append(Token *tok1, Token *tok2) {
   if (!tok1 || tok1->kind == TK_EOF)
@@ -46,6 +61,54 @@ static Token *append(Token *tok1, Token *tok2) {
     cur = cur->next = copy_token(tok1);
   cur->next = tok2;
   return head.next;
+}
+
+// Skip until next `#endif`.
+static Token *skip_cond_incl(Token *tok) {
+  while (tok->kind != TK_EOF) {
+    if (is_hash(tok) && equal(tok->next, "endif"))
+      return tok;
+    tok = tok->next;
+  }
+  return tok;
+}
+
+// Copy all tokens until the next newline, terminate them with
+// an EOF token and then returns them. This function is used to
+// create a new list of tokens for `#if` arguments.
+static Token *copy_line(Token **rest, Token *tok) {
+  Token head = {};
+  Token *cur = &head;
+
+  for (; !tok->at_bol; tok = tok->next)
+    cur = cur->next = copy_token(tok);
+
+  cur->next = new_eof(tok);
+  *rest = tok;
+  return head.next;
+}
+
+// Read and evaluate a constant expression.
+static long eval_const_expr(Token **rest, Token *tok) {
+  Token *start = tok;
+  Token *expr = copy_line(rest, tok->next);
+
+  if (expr->kind == TK_EOF)
+    error_tok(start, "no expression");
+
+  Token *rest2;
+  long val = const_expr(&rest2, expr);
+  if (rest2->kind != TK_EOF)
+    error_tok(rest2, "extra token");
+  return val;
+}
+
+static CondIncl *push_cond_incl(Token *tok) {
+  CondIncl *ci = calloc(1, sizeof(CondIncl));
+  ci->next = cond_incl;
+  ci->tok = tok;
+  cond_incl = ci;
+  return ci;
 }
 
 // Visit all tokens in `tok` while evaluating preprocessing
@@ -62,6 +125,7 @@ static Token *preprocess2(Token *tok) {
       continue;
     }
 
+    Token *start = tok;
     tok = tok->next;
 
     if (equal(tok, "include")) {
@@ -84,6 +148,22 @@ static Token *preprocess2(Token *tok) {
       continue;
     }
 
+    if (equal(tok, "if")) {
+      long val = eval_const_expr(&tok, tok);
+      push_cond_incl(start);
+      if (!val)
+        tok = skip_cond_incl(tok);
+      continue;
+    }
+
+    if (equal(tok, "endif")) {
+      if (!cond_incl)
+        error_tok(start, "stray #endif");
+      cond_incl = cond_incl->next;
+      tok = skip_line(tok->next);
+      continue;
+    }
+
     // `#`-only line is legal. It's called a null directive.
     if (tok->at_bol)
       continue;
@@ -98,6 +178,8 @@ static Token *preprocess2(Token *tok) {
 // Entry point function of the preprocessor.
 Token *preprocess(Token *tok) {
   tok = preprocess2(tok);
+  if (cond_incl)
+    error_tok(cond_incl->tok, "unterminated conditional directive");
   convert_keywords(tok);
   return tok;
 }
