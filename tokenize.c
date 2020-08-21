9e211cbf1d459babf035fd6b3407c2bd184cb639
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-21 17:16:28 +0900

    Report an error on undefined/undeclared functions

--- tokenize.c
+++ tokenize.c
@@ -15,7 +15,7 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
-// Reports an error message in the following format and exit.
+// Reports an error message in the following format.
 //
 // foo.c:10: x = y + 1;
 //               ^ <error message here>
@@ -40,7 +40,6 @@ static void verror_at(int line_no, char *loc, char *fmt, va_list ap) {
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
-  exit(1);
 }
 
 void error_at(char *loc, char *fmt, ...) {
@@ -52,12 +51,14 @@ void error_at(char *loc, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   verror_at(line_no, loc, fmt, ap);
+  exit(1);
 }
 
 void error_tok(Token *tok, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   verror_at(tok->line_no, tok->loc, fmt, ap);
+  exit(1);
 }
 
 // Consumes the current token if it matches `op`.
