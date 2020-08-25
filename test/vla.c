603356cefda3cb43a23f1f5851d08a8b0c220ddc
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-25 09:34:41 +0900

    Support sizeof(typename) where typename is a VLA

--- test/vla.c
+++ test/vla.c
@@ -19,6 +19,8 @@ int main() {
   ASSERT(5, ({ int n=10; int x[n+1][n+6]; int *p=x; for (int i = 0; i<sizeof(x)/4; i++) p[i]=i; x[0][5]; }));
   ASSERT(5*16+2, ({ int n=10; int x[n+1][n+6]; int *p=x; for (int i = 0; i<sizeof(x)/4; i++) p[i]=i; x[5][2]; }));
 
+  ASSERT(10, ({ int n=5; sizeof(char[2][n]); }));
+
   printf("OK\n");
   return 0;
 }
