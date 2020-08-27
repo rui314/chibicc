d3943030668558d33e90aea60751a5225486d38f
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-27 21:51:00 +0900

    Add long type

--- chibicc.h
+++ chibicc.h
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,7 +31,7 @@ typedef struct Token Token;
 struct Token {
   TokenKind kind; // Token kind
   Token *next;    // Next token
-  int val;        // If kind is TK_NUM, its value
+  int64_t val;    // If kind is TK_NUM, its value
   char *loc;      // Token location
   int len;        // Token length
   Type *ty;       // Used if TK_STR
@@ -133,7 +134,7 @@ struct Node {
   Node *args;
 
   Obj *var;      // Used if kind == ND_VAR
-  int val;       // Used if kind == ND_NUM
+  int64_t val;   // Used if kind == ND_NUM
 };
 
 Obj *parse(Token *tok);
@@ -145,6 +146,7 @@ Obj *parse(Token *tok);
 typedef enum {
   TY_CHAR,
   TY_INT,
+  TY_LONG,
   TY_PTR,
   TY_FUNC,
   TY_ARRAY,
@@ -192,6 +194,7 @@ struct Member {
 
 extern Type *ty_char;
 extern Type *ty_int;
+extern Type *ty_long;
 
 bool is_integer(Type *ty);
 Type *copy_type(Type *ty);
--- test/variable.c
+++ test/variable.c
@@ -50,6 +50,8 @@ int main() {
   ASSERT(7, ({ int x; int y; char z; char *a=&y; char *b=&z; b-a; }));
   ASSERT(1, ({ int x; char y; int z; char *a=&y; char *b=&z; b-a; }));
 
+  ASSERT(8, ({ long x; sizeof(x); }));
+
   printf("OK\n");
   return 0;
 }
