c6c852d7c45e6834c23b9f380df59d1b7bcd4019
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-27 21:55:07 +0900

    Add +=, -=, *= and /=

--- tokenize.c
+++ tokenize.c
@@ -307,7 +307,9 @@ static Token *tokenize(char *filename, char *p) {
     // Multi-letter punctuators
     if (startswith(p, "==") || startswith(p, "!=") ||
         startswith(p, "<=") || startswith(p, ">=") ||
-        startswith(p, "->")) {
+        startswith(p, "->") || startswith(p, "+=") ||
+        startswith(p, "-=") || startswith(p, "*=") ||
+        startswith(p, "/=")) {
       cur = cur->next = new_token(TK_RESERVED, p, p + 2);
       p += 2;
       continue;
