14b91f2a36f2e9dcb9d1c1d23aeddff22e63f14a
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-27 21:56:31 +0900

    Add % and %=

--- chibicc.h
+++ chibicc.h
@@ -90,6 +90,7 @@ typedef enum {
   ND_MUL,       // *
   ND_DIV,       // /
   ND_NEG,       // unary -
+  ND_MOD,       // %
   ND_EQ,        // ==
   ND_NE,        // !=
   ND_LT,        // <
--- parse.c
+++ parse.c
@@ -710,7 +710,7 @@ static Node *to_assign(Node *binary) {
 }
 
 // assign    = equality (assign-op assign)?
-// assign-op = "=" | "+=" | "-=" | "*=" | "/="
+// assign-op = "=" | "+=" | "-=" | "*=" | "/=" | "%="
 static Node *assign(Token **rest, Token *tok) {
   Node *node = equality(&tok, tok);
 
@@ -728,6 +728,9 @@ static Node *assign(Token **rest, Token *tok) {
 
   if (equal(tok, "/="))
     return to_assign(new_binary(ND_DIV, node, assign(rest, tok->next), tok));
+
+  if (equal(tok, "%="))
+    return to_assign(new_binary(ND_MOD, node, assign(rest, tok->next), tok));
 
   *rest = tok;
   return node;
@@ -865,7 +868,7 @@ static Node *add(Token **rest, Token *tok) {
   }
 }
 
-// mul = cast ("*" cast | "/" cast)*
+// mul = cast ("*" cast | "/" cast | "%" cast)*
 static Node *mul(Token **rest, Token *tok) {
   Node *node = cast(&tok, tok);
 
@@ -879,6 +882,11 @@ static Node *mul(Token **rest, Token *tok) {
 
     if (equal(tok, "/")) {
       node = new_binary(ND_DIV, node, cast(&tok, tok->next), start);
+      continue;
+    }
+
+    if (equal(tok, "%")) {
+      node = new_binary(ND_MOD, node, cast(&tok, tok->next), start);
       continue;
     }
 
