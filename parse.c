f4ed5b2b8bf2be99b89a12e197d6efe8a3da3ad2
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-27 21:59:19 +0900

    Add continue statement

--- parse.c
+++ parse.c
@@ -77,8 +77,9 @@ static Obj *current_fn;
 static Node *gotos;
 static Node *labels;
 
-// Current "goto" jump target.
+// Current "goto" and "continue" jump targets.
 static char *brk_label;
+static char *cont_label;
 
 static bool is_typename(Token *tok);
 static Type *typespec(Token **rest, Token *tok, VarAttr *attr);
@@ -599,6 +600,7 @@ static bool is_typename(Token *tok) {
 //      | "while" "(" expr ")" stmt
 //      | "goto" ident ";"
 //      | "break" ";"
+//      | "continue" ";"
 //      | ident ":" stmt
 //      | "{" compound-stmt
 //      | expr-stmt
@@ -632,7 +634,9 @@ static Node *stmt(Token **rest, Token *tok) {
     enter_scope();
 
     char *brk = brk_label;
+    char *cont = cont_label;
     brk_label = node->brk_label = new_unique_name();
+    cont_label = node->cont_label = new_unique_name();
 
     if (is_typename(tok)) {
       Type *basety = typespec(&tok, tok, NULL);
@@ -653,6 +657,7 @@ static Node *stmt(Token **rest, Token *tok) {
 
     leave_scope();
     brk_label = brk;
+    cont_label = cont;
     return node;
   }
 
@@ -663,9 +668,14 @@ static Node *stmt(Token **rest, Token *tok) {
     tok = skip(tok, ")");
 
     char *brk = brk_label;
+    char *cont = cont_label;
     brk_label = node->brk_label = new_unique_name();
+    cont_label = node->cont_label = new_unique_name();
+
     node->then = stmt(rest, tok);
+
     brk_label = brk;
+    cont_label = cont;
     return node;
   }
 
@@ -683,6 +693,15 @@ static Node *stmt(Token **rest, Token *tok) {
       error_tok(tok, "stray break");
     Node *node = new_node(ND_GOTO, tok);
     node->unique_label = brk_label;
+    *rest = skip(tok->next, ";");
+    return node;
+  }
+
+  if (equal(tok, "continue")) {
+    if (!cont_label)
+      error_tok(tok, "stray continue");
+    Node *node = new_node(ND_GOTO, tok);
+    node->unique_label = cont_label;
     *rest = skip(tok->next, ";");
     return node;
   }
