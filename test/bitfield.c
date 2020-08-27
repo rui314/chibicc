54c2b3b18fb80235ad9ee53cac3966e8aad9e12a
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-27 23:50:05 +0900

    Handle op=-style assignments to bitfields

--- test/bitfield.c
+++ test/bitfield.c
@@ -34,6 +34,20 @@ int main() {
   ASSERT(0, g46.b);
   ASSERT(0, g46.c);
 
+  typedef struct {
+    int a : 10;
+    int b : 10;
+    int c : 10;
+  } T3;
+
+  ASSERT(1, ({ T3 x={1,2,3}; x.a++; }));
+  ASSERT(2, ({ T3 x={1,2,3}; x.b++; }));
+  ASSERT(3, ({ T3 x={1,2,3}; x.c++; }));
+
+  ASSERT(2, ({ T3 x={1,2,3}; ++x.a; }));
+  ASSERT(3, ({ T3 x={1,2,3}; ++x.b; }));
+  ASSERT(4, ({ T3 x={1,2,3}; ++x.c; }));
+
   printf("OK\n");
   return 0;
 }
