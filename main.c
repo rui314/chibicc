af5e8a98a65ecbf43d3d5df4197c90c100fb0bf6
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-28 01:28:02 +0900

    Run "as" command unless -S is given

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "chibicc.h"
 
+static bool opt_S;
 static bool opt_cc1;
 static bool opt_hash_hash_hash;
 static char *opt_o;
 
 static char *input_path;
+
+static char **tmpfiles;
 
 static void usage(int status) {
   fprintf(stderr, "chibicc [ -o <path> ] <file>\n");
@@ -38,6 +41,11 @@ static void parse_args(int argc, char **argv) {
       continue;
     }
 
+    if (!strcmp(argv[i], "-S")) {
+      opt_S = true;
+      continue;
+    }
+
     if (argv[i][0] == '-' && argv[i][1] != '\0')
       error("unknown argument: %s", argv[i]);
 
@@ -56,6 +64,45 @@ static FILE *open_file(char *path) {
   if (!out)
     error("cannot open output file: %s: %s", path, strerror(errno));
   return out;
+}
+
+// Replace file extension
+static char *replace_extn(char *tmpl, char *extn) {
+  char *filename = basename(strdup(tmpl));
+  int len1 = strlen(filename);
+  int len2 = strlen(extn);
+  char *buf = calloc(1, len1 + len2 + 2);
+
+  char *dot = strrchr(filename, '.');
+  if (dot)
+    *dot = '\0';
+  sprintf(buf, "%s%s", filename, extn);
+  return buf;
+}
+
+static void cleanup(void) {
+  if (tmpfiles)
+    for (int i = 0; tmpfiles[i]; i++)
+      unlink(tmpfiles[i]);
+}
+
+static char *create_tmpfile(void) {
+  char tmpl[] = "/tmp/chibicc-XXXXXX";
+  char *path = calloc(1, sizeof(tmpl));
+  memcpy(path, tmpl, sizeof(tmpl));
+
+  int fd = mkstemp(path);
+  if (fd == -1)
+    error("mkstemp failed: %s", strerror(errno));
+  close(fd);
+
+  static int len = 2;
+  tmpfiles = realloc(tmpfiles, sizeof(char *) * len);
+  tmpfiles[len - 2] = path;
+  tmpfiles[len - 1] = NULL;
+  len++;
+
+  return path;
 }
 
 static void run_subprocess(char **argv) {
@@ -81,10 +128,19 @@ static void run_subprocess(char **argv) {
     exit(1);
 }
 
-static void run_cc1(int argc, char **argv) {
+static void run_cc1(int argc, char **argv, char *input, char *output) {
   char **args = calloc(argc + 10, sizeof(char *));
   memcpy(args, argv, argc * sizeof(char *));
   args[argc++] = "-cc1";
+
+  if (input)
+    args[argc++] = input;
+
+  if (output) {
+    args[argc++] = "-o";
+    args[argc++] = output;
+  }
+
   run_subprocess(args);
 }
 
@@ -99,7 +155,13 @@ static void cc1(void) {
   codegen(prog, out);
 }
 
+static void assemble(char *input, char *output) {
+  char *cmd[] = {"as", "-c", input, "-o", output, NULL};
+  run_subprocess(cmd);
+}
+
 int main(int argc, char **argv) {
+  atexit(cleanup);
   parse_args(argc, argv);
 
   if (opt_cc1) {
@@ -107,6 +169,23 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  run_cc1(argc, argv);
+  char *output;
+  if (opt_o)
+    output = opt_o;
+  else if (opt_S)
+    output = replace_extn(input_path, ".s");
+  else
+    output = replace_extn(input_path, ".o");
+
+  // If -S is given, assembly text is the final output.
+  if (opt_S) {
+    run_cc1(argc, argv, input_path, output);
+    return 0;
+  }
+
+  // Otherwise, run the assembler to assemble our output.
+  char *tmpfile = create_tmpfile();
+  run_cc1(argc, argv, input_path, tmpfile);
+  assemble(tmpfile, output);
   return 0;
 }
