a74a63a4d9ec2860c2818e753ff872628b4500ee
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-28 23:29:49 +0900

    Add unsigned integral types

--- test/cast.c
+++ test/cast.c
@@ -11,6 +11,36 @@ int main() {
 
   (void)1;
 
+  ASSERT(-1, (char)255);
+  ASSERT(-1, (signed char)255);
+  ASSERT(255, (unsigned char)255);
+  ASSERT(-1, (short)65535);
+  ASSERT(65535, (unsigned short)65535);
+  ASSERT(-1, (int)0xffffffff);
+  ASSERT(0xffffffff, (unsigned)0xffffffff);
+
+  ASSERT(1, -1<1);
+  ASSERT(0, -1<(unsigned)1);
+  ASSERT(254, (char)127+(char)127);
+  ASSERT(65534, (short)32767+(short)32767);
+  ASSERT(-1, -1>>1);
+  ASSERT(-1, (unsigned long)-1);
+  ASSERT(2147483647, ((unsigned)-1)>>1);
+  ASSERT(-50, (-100)/2);
+  ASSERT(2147483598, ((unsigned)-100)/2);
+  ASSERT(9223372036854775758, ((unsigned long)-100)/2);
+  ASSERT(0, ((long)-1)/(unsigned)100);
+  ASSERT(-2, (-100)%7);
+  ASSERT(2, ((unsigned)-100)%7);
+  ASSERT(6, ((unsigned long)-100)%9);
+
+  ASSERT(65535, (int)(unsigned short)65535);
+  ASSERT(65535, ({ unsigned short x = 65535; x; }));
+  ASSERT(65535, ({ unsigned short x = 65535; (int)x; }));
+
+  ASSERT(-1, ({ typedef short T; T x = 65535; (int)x; }));
+  ASSERT(65535, ({ typedef unsigned short T; T x = 65535; (int)x; }));
+
   printf("OK\n");
   return 0;
 }
--- tokenize.c
+++ tokenize.c
@@ -120,6 +120,7 @@ static bool is_keyword(Token *tok) {
     "struct", "union", "short", "long", "void", "typedef", "_Bool",
     "enum", "static", "goto", "break", "continue", "switch", "case",
     "default", "extern", "_Alignof", "_Alignas", "do", "signed",
+    "unsigned",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
@@ -234,7 +235,7 @@ static Token *read_int_literal(char *start) {
     base = 8;
   }
 
-  long val = strtoul(p, &p, base);
+  int64_t val = strtoul(p, &p, base);
   if (isalnum(*p))
     error_at(p, "invalid digit");
 
