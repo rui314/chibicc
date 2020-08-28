3435248c340b5719e13c0f3e8dbca496cb15d5c0
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-28 23:29:49 +0900

    Add unsigned integral types

--- parse.c
+++ parse.c
@@ -377,14 +377,15 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
   // keyword "void" so far. With this, we can use a switch statement
   // as you can see below.
   enum {
-    VOID   = 1 << 0,
-    BOOL   = 1 << 2,
-    CHAR   = 1 << 4,
-    SHORT  = 1 << 6,
-    INT    = 1 << 8,
-    LONG   = 1 << 10,
-    OTHER  = 1 << 12,
-    SIGNED = 1 << 13,
+    VOID     = 1 << 0,
+    BOOL     = 1 << 2,
+    CHAR     = 1 << 4,
+    SHORT    = 1 << 6,
+    INT      = 1 << 8,
+    LONG     = 1 << 10,
+    OTHER    = 1 << 12,
+    SIGNED   = 1 << 13,
+    UNSIGNED = 1 << 14,
   };
 
   Type *ty = ty_int;
@@ -458,6 +459,8 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
       counter += LONG;
     else if (equal(tok, "signed"))
       counter |= SIGNED;
+    else if (equal(tok, "unsigned"))
+      counter |= UNSIGNED;
     else
       unreachable();
 
@@ -472,16 +475,27 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
     case SIGNED + CHAR:
       ty = ty_char;
       break;
+    case UNSIGNED + CHAR:
+      ty = ty_uchar;
+      break;
     case SHORT:
     case SHORT + INT:
     case SIGNED + SHORT:
     case SIGNED + SHORT + INT:
       ty = ty_short;
       break;
+    case UNSIGNED + SHORT:
+    case UNSIGNED + SHORT + INT:
+      ty = ty_ushort;
+      break;
     case INT:
     case SIGNED:
     case SIGNED + INT:
       ty = ty_int;
+      break;
+    case UNSIGNED:
+    case UNSIGNED + INT:
+      ty = ty_uint;
       break;
     case LONG:
     case LONG + INT:
@@ -492,6 +506,12 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
     case SIGNED + LONG + LONG:
     case SIGNED + LONG + LONG + INT:
       ty = ty_long;
+      break;
+    case UNSIGNED + LONG:
+    case UNSIGNED + LONG + INT:
+    case UNSIGNED + LONG + LONG:
+    case UNSIGNED + LONG + LONG + INT:
+      ty = ty_ulong;
       break;
     default:
       error_tok(tok, "invalid type");
@@ -1075,7 +1095,7 @@ static void gvar_initializer(Token **rest, Token *tok, Obj *var) {
 static bool is_typename(Token *tok) {
   static char *kw[] = {
     "void", "_Bool", "char", "short", "int", "long", "struct", "union",
-    "typedef", "enum", "static", "extern", "_Alignas", "signed",
+    "typedef", "enum", "static", "extern", "_Alignas", "signed", "unsigned",
   };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
