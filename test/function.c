e0bf168041ef60687b5d4454a93fc78c4f3acc48
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-29 10:37:53 +0900

    Add long double

--- test/function.c
+++ test/function.c
@@ -201,6 +201,14 @@ inline int inline_fn(void) {
   return 3;
 }
 
+double to_double(long double x) {
+  return x;
+}
+
+long double to_ldouble(int x) {
+  return x;
+}
+
 int main() {
   ASSERT(3, ret3());
   ASSERT(8, add2(3, 5));
@@ -370,6 +378,17 @@ int main() {
   ASSERT(5, (***add2)(2,3));
 
   ASSERT(3, inline_fn());
+
+  ASSERT(0, ({ char buf[100]; sprintf(buf, "%Lf", (long double)12.3); strncmp(buf, "12.3", 4); }));
+
+  ASSERT(1, to_double(3.5) == 3.5);
+  ASSERT(0, to_double(3.5) == 3);
+
+  ASSERT(1, (long double)5.0 == (long double)5.0);
+  ASSERT(0, (long double)5.0 == (long double)5.2);
+
+  ASSERT(1, to_ldouble(5.0) == 5.0);
+  ASSERT(0, to_ldouble(5.0) == 5.2);
 
   printf("OK\n");
 }
