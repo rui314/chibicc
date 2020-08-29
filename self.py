656c55a90a74b0f04a8bf18525c8984843051ece
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-30 01:04:23 +0900

    Add macro stringizing operator (#)

--- self.py
+++ self.py
@@ -76,6 +76,7 @@ int wait(int *wstatus);
 int atexit(void (*)(void));
 FILE *open_memstream(char **ptr, size_t *sizeloc);
 char *dirname(char *path);
+char *strncpy(char *dest, char *src, long n);
 """)
 
 for path in sys.argv[1:]:
