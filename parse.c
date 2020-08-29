a057954aedf523e2656dfc69f55822963f92e459
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-29 10:37:53 +0900

    Add long double

--- parse.c
+++ parse.c
@@ -564,8 +564,10 @@ static Type *typespec(Token **rest, Token *tok, VarAttr *attr) {
       ty = ty_float;
       break;
     case DOUBLE:
-    case LONG + DOUBLE:
       ty = ty_double;
+      break;
+    case LONG + DOUBLE:
+      ty = ty_ldouble;
       break;
     default:
       error_tok(tok, "invalid type");
