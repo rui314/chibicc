c0b5931b519f2ceb71255d0377a3e84fd8f1f509
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-30 02:33:01 +0900

    Preserve newline and space during macro expansion

--- test/macro.c
+++ test/macro.c
@@ -290,6 +290,22 @@ int main() {
 #endif
   ASSERT(5, m);
 
+#define STR(x) #x
+#define M12(x) STR(x)
+#define M13(x) M12(foo.x)
+  ASSERT(0, strcmp(M13(bar), "foo.bar"));
+
+#define M13(x) M12(foo. x)
+  ASSERT(0, strcmp(M13(bar), "foo. bar"));
+
+#define M12 foo
+#define M13(x) STR(x)
+#define M14(x) M13(x.M12)
+  ASSERT(0, strcmp(M14(bar), "bar.foo"));
+
+#define M14(x) M13(x. M12)
+  ASSERT(0, strcmp(M14(bar), "bar. foo"));
+
   printf("OK\n");
   return 0;
 }
