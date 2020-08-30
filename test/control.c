5450a677e00de2a0fedff278edc5413ec6690962
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-30 12:51:05 +0900

    [GNU] Support case ranges

--- test/control.c
+++ test/control.c
@@ -83,6 +83,10 @@ int main() {
   ASSERT(10, ({ double i=10.0; int j=0; for (; i; i--, j++); j; }));
   ASSERT(10, ({ double i=10.0; int j=0; do j++; while(--i); j; }));
 
+  ASSERT(2, ({ int i=0; switch(7) { case 0 ... 5: i=1; break; case 6 ... 20: i=2; break; } i; }));
+  ASSERT(1, ({ int i=0; switch(7) { case 0 ... 7: i=1; break; case 8 ... 10: i=2; break; } i; }));
+  ASSERT(1, ({ int i=0; switch(7) { case 0: i=1; break; case 7 ... 7: i=1; break; } i; }));
+
   printf("OK\n");
   return 0;
 }
