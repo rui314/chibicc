88a552998e6be5fe83a481490ea2d8788fb2035a
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-30 18:57:20 +0900

    Add #include <...>

--- test/macro.c
+++ test/macro.c
@@ -306,6 +306,22 @@ int main() {
 #define M14(x) M13(x. M12)
   ASSERT(0, strcmp(M14(bar), "bar. foo"));
 
+#include "include3.h"
+  ASSERT(3, foo);
+
+#include "include4.h"
+  ASSERT(4, foo);
+
+#define M13 "include3.h"
+#include M13
+  ASSERT(3, foo);
+
+#define M13 < include4.h
+#include M13 >
+  ASSERT(4, foo);
+
+#undef foo
+
   printf("OK\n");
   return 0;
 }
