84e846ef4f684defef7540930a44963e945335e5
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-30 18:58:41 +0900

    Add stdarg.h, stdbool.h, stddef.h, stdalign.h and float.h

---
+++ include/stddef.h
@@ -0,0 +1,11 @@
+#ifndef __STDDEF_H
+#define __STDDEF_H
+
+#define NULL ((void *)0)
+
+typedef unsigned long size_t;
+typedef long ptrdiff_t;
+typedef unsigned int wchar_t;
+typedef long max_align_t;
+
+#endif
