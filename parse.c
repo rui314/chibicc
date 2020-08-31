c2b6db8e40063eb2707d162ab7e304739d9f6332
Rui Ueyama <ruiu@cs.stanford.edu>
2020-08-31 21:28:53 +0900

    Decay a function to a pointer in the func param context

--- parse.c
+++ parse.c
@@ -579,11 +579,17 @@ static Type *func_params(Token **rest, Token *tok, Type *ty) {
     Type *ty2 = typespec(&tok, tok, NULL);
     ty2 = declarator(&tok, tok, ty2);
 
-    // "array of T" is converted to "pointer to T" only in the parameter
-    // context. For example, *argv[] is converted to **argv by this.
+    Token *name = ty2->name;
+
     if (ty2->kind == TY_ARRAY) {
-      Token *name = ty2->name;
+      // "array of T" is converted to "pointer to T" only in the parameter
+      // context. For example, *argv[] is converted to **argv by this.
       ty2 = pointer_to(ty2->base);
+      ty2->name = name;
+    } else if (ty2->kind == TY_FUNC) {
+      // Likewise, a function is converted to a pointer to a function
+      // only in the parameter context.
+      ty2 = pointer_to(ty2);
       ty2->name = name;
     }
 
