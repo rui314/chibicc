7ab9353d5f70b77510b0c809880663292852a643
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-03 01:22:17 +0900

    Ignore const, volatile, auto, register, restrict or _Noreturn.

---
+++ test/const.c
@@ -0,0 +1,14 @@
+#include "test.h"
+
+int main() {
+  { const x; }
+  { int const x; }
+  { const int x; }
+  { const int const const x; }
+  ASSERT(5, ({ const x = 5; x; }));
+  ASSERT(8, ({ const x = 8; int *const y=&x; *y; }));
+  ASSERT(6, ({ const x = 6; *(const * const)&x; }));
+
+  printf("OK\n");
+  return 0;
+}
