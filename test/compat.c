ace64171e64a2adf283c9a90862ac5f760f4a356
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-03 01:22:17 +0900

    Ignore const, volatile, auto, register, restrict or _Noreturn.

---
+++ test/compat.c
@@ -0,0 +1,17 @@
+#include "test.h"
+
+_Noreturn noreturn_fn(int restrict x) {
+  exit(0);
+}
+
+int main() {
+  { volatile x; }
+  { int volatile x; }
+  { volatile int x; }
+  { volatile int volatile volatile x; }
+  { int volatile * volatile volatile x; }
+  { auto ** restrict __restrict __restrict__ const volatile *x; }
+
+  printf("OK\n");
+  return 0;
+}
