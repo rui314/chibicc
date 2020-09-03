0aa87110b1e8d50305f92129193a2f7e958e1c1a
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-03 15:43:36 +0900

    Add keyword "int" and make variable definition mandatory

--- type.c
+++ type.c
@@ -41,18 +41,19 @@ void add_type(Node *node) {
   case ND_NE:
   case ND_LT:
   case ND_LE:
-  case ND_VAR:
   case ND_NUM:
     node->ty = ty_int;
+    return;
+  case ND_VAR:
+    node->ty = node->var->ty;
     return;
   case ND_ADDR:
     node->ty = pointer_to(node->lhs->ty);
     return;
   case ND_DEREF:
-    if (node->lhs->ty->kind == TY_PTR)
-      node->ty = node->lhs->ty->base;
-    else
-      node->ty = ty_int;
+    if (node->lhs->ty->kind != TY_PTR)
+      error_tok(node->tok, "invalid pointer dereference");
+    node->ty = node->lhs->ty->base;
     return;
   }
 }
