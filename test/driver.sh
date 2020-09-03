d128f728cf97171629851a6b035a12d99b0b03e3
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-03 19:34:07 +0900

    Add -M option

--- test/driver.sh
+++ test/driver.sh
@@ -217,4 +217,11 @@ check '.so'
 $chibicc -hashmap-test
 check 'hashmap'
 
+# -M
+echo '#include "out2.h"' > $tmp/out.c
+echo '#include "out3.h"' >> $tmp/out.c
+touch $tmp/out2.h $tmp/out3.h
+$chibicc -M -I$tmp $tmp/out.c | grep -q -z '^out.o: .*/out\.c .*/out2\.h .*/out3\.h'
+check -M
+
 echo OK
