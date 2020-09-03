3b30177f6bcd70ae99d62a0ea4d8aaa31c05a531
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-03 15:37:46 +0900

    Add "return" statement

--- codegen.c
+++ codegen.c
@@ -94,7 +94,12 @@ static void gen_expr(Node *node) {
 }
 
 static void gen_stmt(Node *node) {
-  if (node->kind == ND_EXPR_STMT) {
+  switch (node->kind) {
+  case ND_RETURN:
+    gen_expr(node->lhs);
+    printf("  jmp .L.return\n");
+    return;
+  case ND_EXPR_STMT:
     gen_expr(node->lhs);
     return;
   }
@@ -128,6 +133,7 @@ void codegen(Function *prog) {
     assert(depth == 0);
   }
 
+  printf(".L.return:\n");
   printf("  mov %%rbp, %%rsp\n");
   printf("  pop %%rbp\n");
   printf("  ret\n");
--- parse.c
+++ parse.c
@@ -61,8 +61,15 @@ static Obj *new_lvar(char *name) {
   return var;
 }
 
-// stmt = expr-stmt
+// stmt = "return" expr ";"
+//      | expr-stmt
 static Node *stmt(Token **rest, Token *tok) {
+  if (equal(tok, "return")) {
+    Node *node = new_unary(ND_RETURN, expr(&tok, tok->next));
+    *rest = skip(tok, ";");
+    return node;
+  }
+
   return expr_stmt(rest, tok);
 }
 
