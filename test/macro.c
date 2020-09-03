d367510fcc1396fa252c4b87439c2f9fcd0abbe7
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-03 19:24:23 +0900

    Add #include "..."

--- test/macro.c
+++ test/macro.c
@@ -4,11 +4,16 @@ int sprintf(char *buf, char *fmt, ...);
 int strcmp(char *p, char *q);
 int memcmp(char *p, char *q, long n);
 
+#include "include1.h"
+
 #
 
 /* */ #
 
 int main() {
+  assert(5, include1, "include1");
+  assert(7, include2, "include2");
+
   printf("OK\n");
   return 0;
 }
