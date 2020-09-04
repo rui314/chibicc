f26c8c26aae80d8a436677df86ca0856370e1ecc
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 13:39:06 +0900

    Make pointer arithmetic work

--- chibicc.h
+++ chibicc.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Type Type;
 typedef struct Node Node;
 
 //
@@ -84,6 +85,7 @@ typedef enum {
 struct Node {
   NodeKind kind; // Node kind
   Node *next;    // Next node
+  Type *ty;      // Type, e.g. int or pointer to int
   Token *tok;    // Representative token
 
   Node *lhs;     // Left-hand side
@@ -104,6 +106,25 @@ struct Node {
 };
 
 Function *parse(Token *tok);
+
+//
+// type.c
+//
+
+typedef enum {
+  TY_INT,
+  TY_PTR,
+} TypeKind;
+
+struct Type {
+  TypeKind kind;
+  Type *base;
+};
+
+extern Type *ty_int;
+
+bool is_integer(Type *ty);
+void add_type(Node *node);
 
 //
 // codegen.c
