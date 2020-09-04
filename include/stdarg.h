73f5e53281c385f891706055860713f357519855
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 18:27:31 +0900

    Allow variadic function to take more than 6 parameters

--- include/stdarg.h
+++ include/stdarg.h
@@ -15,28 +15,38 @@ typedef __va_elem va_list[1];
 
 #define va_end(ap)
 
-static void *__va_arg_gp(__va_elem *ap) {
-  void *r = (char *)ap->reg_save_area + ap->gp_offset;
+static void *__va_arg_mem(__va_elem *ap, int sz, int align) {
+  void *p = ap->overflow_arg_area;
+  if (align > 8)
+    p = (p + 15) / 16 * 16;
+  ap->overflow_arg_area = ((unsigned long)p + sz + 7) / 8 * 8;
+  return p;
+}
+
+static void *__va_arg_gp(__va_elem *ap, int sz, int align) {
+  if (ap->gp_offset >= 48)
+    return __va_arg_mem(ap, sz, align);
+
+  void *r = ap->reg_save_area + ap->gp_offset;
   ap->gp_offset += 8;
   return r;
 }
 
-static void *__va_arg_fp(__va_elem *ap) {
-  void *r = (char *)ap->reg_save_area + ap->fp_offset;
+static void *__va_arg_fp(__va_elem *ap, int sz, int align) {
+  if (ap->fp_offset >= 112)
+    return __va_arg_mem(ap, sz, align);
+
+  void *r = ap->reg_save_area + ap->fp_offset;
   ap->fp_offset += 8;
   return r;
 }
 
-static void *__va_arg_mem(__va_elem *ap) {
-  1 / 0; // not implemented
-}
-
-#define va_arg(ap, type)                        \
-  ({                                            \
-    int klass = __builtin_reg_class(type);      \
-    *(type *)(klass == 0 ? __va_arg_gp(ap) :    \
-              klass == 1 ? __va_arg_fp(ap) :    \
-              __va_arg_mem(ap));                \
+#define va_arg(ap, ty)                                                  \
+  ({                                                                    \
+    int klass = __builtin_reg_class(ty);                                \
+    *(ty *)(klass == 0 ? __va_arg_gp(ap, sizeof(ty), _Alignof(ty)) :    \
+            klass == 1 ? __va_arg_fp(ap, sizeof(ty), _Alignof(ty)) :    \
+            __va_arg_mem(ap, sizeof(ty), _Alignof(ty)));                \
   })
 
 #define __GNUC_VA_LIST 1
--- test/varargs.c
+++ test/varargs.c
@@ -28,9 +28,20 @@ int sum2(int x, ...) {
   }
 }
 
+char *fmt(char *buf, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vsprintf(buf, fmt, ap);
+  va_end(ap);
+  return buf;
+}
+
 int main() {
   ASSERT(6, sum1(1, 2, 3, 0));
+  ASSERT(55, sum1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0));
   ASSERT(21, sum2(1, 2.0, 3, 4.0, 5, 6.0, 0));
+  ASSERT(21, sum2(1, 2.0, 3, 4.0, 5, 6.0, 0));
+  ASSERT(210, sum2(1, 2.0, 3, 4.0, 5, 6.0, 7, 8.0, 9, 10.0, 11, 12.0, 13, 14.0, 15, 16.0, 17, 18.0, 19, 20.0, 0));
 
   printf("OK\n");
   return 0;
