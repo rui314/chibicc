eb9da30e544e9abf969a774326dcb881ff5ef864
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 12:27:21 +0900

    Add goto and labeled statement

--- test/control.c
+++ test/control.c
@@ -36,6 +36,10 @@ int main() {
   ASSERT(0, (2-2)&&5);
   ASSERT(1, 1&&5);
 
+  ASSERT(3, ({ int i=0; goto a; a: i++; b: i++; c: i++; i; }));
+  ASSERT(2, ({ int i=0; goto e; d: i++; e: i++; f: i++; i; }));
+  ASSERT(1, ({ int i=0; goto i; g: i++; h: i++; i: i++; i; }));
+
   printf("OK\n");
   return 0;
 }
