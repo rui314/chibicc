5aefcd19cb4104de3c9c6a6b29e6f5bfb47a480a
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 17:58:53 +0900

    Merge Function with Var
    No functional change

--- chibicc.h
+++ chibicc.h
@@ -44,22 +44,22 @@ Token *tokenize(char *input);
 // parse.c
 //
 
-// Local variable
+// Variable or function
 typedef struct Obj Obj;
 struct Obj {
   Obj *next;
-  char *name; // Variable name
-  Type *ty;   // Type
-  int offset; // Offset from RBP
-};
+  char *name;    // Variable name
+  Type *ty;      // Type
+  bool is_local; // local or global/function
 
-// Function
-typedef struct Function Function;
-struct Function {
-  Function *next;
-  char *name;
+  // Local variable
+  int offset;
+
+  // Global variable or function
+  bool is_function;
+
+  // Function
   Obj *params;
-
   Node *body;
   Obj *locals;
   int stack_size;
@@ -117,7 +117,7 @@ struct Node {
   int val;       // Used if kind == ND_NUM
 };
 
-Function *parse(Token *tok);
+Obj *parse(Token *tok);
 
 //
 // type.c
@@ -169,4 +169,4 @@ void add_type(Node *node);
 // codegen.c
 //
 
-void codegen(Function *prog);
+void codegen(Obj *prog);
