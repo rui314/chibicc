0c401f5f341c942287047e1d64009cac33b2fe13
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 12:27:21 +0900

    Add goto and labeled statement

--- chibicc.h
+++ chibicc.h
@@ -111,6 +111,8 @@ typedef enum {
   ND_IF,        // "if"
   ND_FOR,       // "for" or "while"
   ND_BLOCK,     // { ... }
+  ND_GOTO,      // "goto"
+  ND_LABEL,     // Labeled statement
   ND_FUNCALL,   // Function call
   ND_EXPR_STMT, // Expression statement
   ND_STMT_EXPR, // Statement expression
@@ -146,6 +148,11 @@ struct Node {
   char *funcname;
   Type *func_ty;
   Node *args;
+
+  // Goto or labeled statement
+  char *label;
+  char *unique_label;
+  Node *goto_next;
 
   Obj *var;      // Used if kind == ND_VAR
   int64_t val;   // Used if kind == ND_NUM
