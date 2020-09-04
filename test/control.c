bc0c468a42081d1e0653ef3b6e56db3ca839a302
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 19:03:01 +0900

    Rewrite tests in shell script in C

---
+++ test/control.c
@@ -0,0 +1,25 @@
+#include "test.h"
+
+/*
+ * This is a block comment.
+ */
+
+int main() {
+  ASSERT(3, ({ int x; if (0) x=2; else x=3; x; }));
+  ASSERT(3, ({ int x; if (1-1) x=2; else x=3; x; }));
+  ASSERT(2, ({ int x; if (1) x=2; else x=3; x; }));
+  ASSERT(2, ({ int x; if (2-1) x=2; else x=3; x; }));
+
+  ASSERT(55, ({ int i=0; int j=0; for (i=0; i<=10; i=i+1) j=i+j; j; }));
+
+  ASSERT(10, ({ int i=0; while(i<10) i=i+1; i; }));
+
+  ASSERT(3, ({ 1; {2;} 3; }));
+  ASSERT(5, ({ ;;; 5; }));
+
+  ASSERT(10, ({ int i=0; while(i<10) i=i+1; i; }));
+  ASSERT(55, ({ int i=0; int j=0; while(i<=10) {j=i+j; i=i+1;} j; }));
+
+  printf("OK\n");
+  return 0;
+}
---
+++ test/string.c
@@ -0,0 +1,40 @@
+#include "test.h"
+
+int main() {
+  ASSERT(0, ""[0]);
+  ASSERT(1, sizeof(""));
+
+  ASSERT(97, "abc"[0]);
+  ASSERT(98, "abc"[1]);
+  ASSERT(99, "abc"[2]);
+  ASSERT(0, "abc"[3]);
+  ASSERT(4, sizeof("abc"));
+
+  ASSERT(7, "\a"[0]);
+  ASSERT(8, "\b"[0]);
+  ASSERT(9, "\t"[0]);
+  ASSERT(10, "\n"[0]);
+  ASSERT(11, "\v"[0]);
+  ASSERT(12, "\f"[0]);
+  ASSERT(13, "\r"[0]);
+  ASSERT(27, "\e"[0]);
+
+  ASSERT(106, "\j"[0]);
+  ASSERT(107, "\k"[0]);
+  ASSERT(108, "\l"[0]);
+
+  ASSERT(7, "\ax\ny"[0]);
+  ASSERT(120, "\ax\ny"[1]);
+  ASSERT(10, "\ax\ny"[2]);
+  ASSERT(121, "\ax\ny"[3]);
+
+  ASSERT(0, "\0"[0]);
+  ASSERT(16, "\20"[0]);
+  ASSERT(65, "\101"[0]);
+  ASSERT(104, "\1500"[0]);
+  ASSERT(0, "\x00"[0]);
+  ASSERT(119, "\x77"[0]);
+
+  printf("OK\n");
+  return 0;
+}
