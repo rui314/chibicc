94ed9371e060eade72808375951879d8feac33aa
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 14:41:55 +0900

    Add static local variables

--- parse.c
+++ parse.c
@@ -673,6 +673,15 @@ static Node *declaration(Token **rest, Token *tok, Type *basety, VarAttr *attr) 
     if (ty->kind == TY_VOID)
       error_tok(tok, "variable declared void");
 
+    if (attr && attr->is_static) {
+      // static local variable
+      Var *var = new_anon_gvar(ty);
+      push_scope(get_ident(ty->name))->var = var;
+      if (equal(tok, "="))
+        gvar_initializer(&tok, tok->next, var);
+      continue;
+    }
+
     Var *var = new_lvar(get_ident(ty->name), ty);
     if (attr && attr->align)
       var->align = attr->align;
