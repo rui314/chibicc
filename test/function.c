5eec02c1dc3cfa2315917d91c26ddad585e51cc9
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 11:56:28 +0900

    [GNU] Add __FUNCTION__

--- test/function.c
+++ test/function.c
@@ -118,6 +118,10 @@ char *func_fn(void) {
   return __func__;
 }
 
+char *function_fn(void) {
+  return __FUNCTION__;
+}
+
 int main() {
   ASSERT(3, ret3());
   ASSERT(8, add2(3, 5));
@@ -198,6 +202,8 @@ int main() {
   ASSERT(5, sizeof(__func__));
   ASSERT(0, strcmp("main", __func__));
   ASSERT(0, strcmp("func_fn", func_fn()));
+  ASSERT(0, strcmp("main", __FUNCTION__));
+  ASSERT(0, strcmp("function_fn", function_fn()));
 
   printf("OK\n");
   return 0;
