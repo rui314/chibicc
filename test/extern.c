27647455e4cb7db1545a7b69c3a324aa025a471a
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-04 18:08:32 +0900

    Handle extern declarations in a block

--- test/extern.c
+++ test/extern.c
@@ -7,6 +7,15 @@ int main() {
   ASSERT(5, ext1);
   ASSERT(5, *ext2);
 
+  extern int ext3;
+  ASSERT(7, ext3);
+
+  int ext_fn1(int x);
+  ASSERT(5, ext_fn1(5));
+
+  extern int ext_fn2(int x);
+  ASSERT(8, ext_fn2(8));
+
   printf("OK\n");
   return 0;
 }
