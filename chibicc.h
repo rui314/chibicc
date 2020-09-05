549373b3bd20d5c9bc0d73b078f8025ad2992541
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-06 08:10:01 +0900

    Add short type

--- chibicc.h
+++ chibicc.h
@@ -144,6 +144,7 @@ Var *parse(Token *tok);
 
 typedef enum {
   TY_CHAR,
+  TY_SHORT,
   TY_INT,
   TY_LONG,
   TY_PTR,
@@ -192,6 +193,7 @@ struct Member {
 };
 
 extern Type *ty_char;
+extern Type *ty_short;
 extern Type *ty_int;
 extern Type *ty_long;
 
