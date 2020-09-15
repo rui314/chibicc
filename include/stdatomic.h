1bb084e23ca1f06b9a9184679b107e7da34b3760
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-15 11:30:56 +0900

    Add atomic_exchange

--- include/stdatomic.h
+++ include/stdatomic.h
@@ -7,4 +7,7 @@
 #define atomic_compare_exchange_strong(p, old, new) \
   __builtin_compare_and_swap((p), (old), (new))
 
+#define atomic_exchange(obj, val) __builtin_atomic_exchange(obj, val)
+#define atomic_exchange_explicit(obj, val, order) __builtin_atomic_exchange(obj, val)
+
 #endif
