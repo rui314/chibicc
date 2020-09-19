aea8f248a29fc3ec9e597ff5a29e8a2b1af2f72e
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-19 20:42:21 +0900

    Add default include paths

--- self.py
+++ self.py
@@ -54,6 +54,7 @@ int strncmp(char *p, char *q, long n);
 void *memcpy(char *dst, char *src, long n);
 char *strdup(char *p);
 char *strndup(char *p, long n);
+char *strdup(char *p);
 int isspace(int c);
 int ispunct(int c);
 int isdigit(int c);
