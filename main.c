a939a7a90638631c296dfb63d857b24555b25327
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-19 20:42:21 +0900

    Add default include paths

--- main.c
+++ main.c
@@ -29,6 +29,17 @@ static bool take_arg(char *arg) {
   return false;
 }
 
+static void add_default_include_paths(char *argv0) {
+  // We expect that chibicc-specific include files are installed
+  // to ./include relative to argv[0].
+  strarray_push(&include_paths, format("%s/include", dirname(strdup(argv0))));
+
+  // Add standard include paths.
+  strarray_push(&include_paths, "/usr/local/include");
+  strarray_push(&include_paths, "/usr/include/x86_64-linux-gnu");
+  strarray_push(&include_paths, "/usr/include");
+}
+
 static void parse_args(int argc, char **argv) {
   // Make sure that all command line options that take an argument
   // have an argument.
@@ -313,6 +324,7 @@ int main(int argc, char **argv) {
   parse_args(argc, argv);
 
   if (opt_cc1) {
+    add_default_include_paths(argv[0]);
     cc1();
     return 0;
   }
