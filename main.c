ecdcea180a3745e9aa0ebac978abf5dd50d264ca
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-19 21:09:17 +0900

    Add -Wl, option

--- main.c
+++ main.c
@@ -211,7 +211,7 @@ static void parse_args(int argc, char **argv) {
       continue;
     }
 
-    if (!strncmp(argv[i], "-l", 2)) {
+    if (!strncmp(argv[i], "-l", 2) || !strncmp(argv[i], "-Wl,", 4)) {
       strarray_push(&input_paths, argv[i]);
       continue;
     }
@@ -684,6 +684,16 @@ int main(int argc, char **argv) {
 
     if (!strncmp(input, "-l", 2)) {
       strarray_push(&ld_args, input);
+      continue;
+    }
+
+    if (!strncmp(input, "-Wl,", 4)) {
+      char *s = strdup(input + 4);
+      char *arg = strtok(s, ",");
+      while (arg) {
+        strarray_push(&ld_args, arg);
+        arg = strtok(NULL, ",");
+      }
       continue;
     }
 
