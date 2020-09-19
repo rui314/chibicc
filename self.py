e57f20590d1ce64862d40ad6aefef8969922648d
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-19 20:36:26 +0900

    Run "ld" unless -c is given

--- self.py
+++ self.py
@@ -32,6 +32,13 @@ struct stat {
   char _[512];
 };
 
+typedef struct {
+  size_t gl_pathc;
+  char **gl_pathv;
+  size_t gl_offs;
+  char _[512];
+} glob_t;
+
 void *malloc(long size);
 void *calloc(long nmemb, long size);
 void *realloc(void *buf, long size);
@@ -46,6 +53,10 @@ int fclose(FILE *fp);
 int fputc(int c, FILE *stream);
 int feof(FILE *stream);
 static void assert() {}
+int glob(char *pattern, int flags, void *errfn, glob_t *pglob);
+void globfree(glob_t *pglob);
+int stat(char *pathname, struct stat *statbuf);
+char *dirname(char *path);
 int strcmp(char *s1, char *s2);
 int strncasecmp(char *s1, char *s2, long n);
 int memcmp(char *s1, char *s2, long n);
