9bc5c64b83bd20d58b557a188dd0003c21a9f6b0
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-20 13:29:23 +0900

    Add -Xlinker option

--- test/driver.sh
+++ test/driver.sh
@@ -302,4 +302,11 @@ echo 'int main() {}' | $chibicc -c -o $tmp/baz.o -xc -
 cc -Wl,-z,muldefs,--gc-sections -o $tmp/foo $tmp/foo.o $tmp/bar.o $tmp/baz.o
 check -Wl,
 
+# -Xlinker
+echo 'int foo() {}' | $chibicc -c -o $tmp/foo.o -xc -
+echo 'int foo() {}' | $chibicc -c -o $tmp/bar.o -xc -
+echo 'int main() {}' | $chibicc -c -o $tmp/baz.o -xc -
+cc -Xlinker -z -Xlinker muldefs -Xlinker --gc-sections -o $tmp/foo $tmp/foo.o $tmp/bar.o $tmp/baz.o
+check -Xlinker
+
 echo OK
