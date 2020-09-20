6a75e3555f4a0b0a459eb10c52b3848ec143d68f
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-20 21:19:08 +0900

    check the number of function arguments

--- parse.c
+++ parse.c
@@ -633,6 +633,9 @@ static Type *func_params(Token **rest, Token *tok, Type *ty) {
     cur = cur->next = copy_type(ty2);
   }
 
+  if (cur == &head)
+    is_variadic = true;
+
   ty = func_type(ty);
   ty->params = head.next;
   ty->is_variadic = is_variadic;
@@ -2848,6 +2851,9 @@ static Node *funcall(Token **rest, Token *tok, Node *fn) {
 
     Node *arg = assign(&tok, tok);
     add_type(arg);
+
+    if (!param_ty && !ty->is_variadic)
+      error_tok(tok, "too many arguments to function call");
 
     if (param_ty) {
       if (param_ty->kind != TY_STRUCT && param_ty->kind != TY_UNION)
