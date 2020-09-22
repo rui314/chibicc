9f23df7ff6f51238c22e4adb5011374621ed5111
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-22 18:28:37 +0900

    Add flonum ==, !=, < and <=

--- type.c
+++ type.c
@@ -73,6 +73,11 @@ static Type *get_common_type(Type *ty1, Type *ty2) {
   if (ty1->base)
     return pointer_to(ty1->base);
 
+  if (ty1->kind == TY_DOUBLE || ty2->kind == TY_DOUBLE)
+    return ty_double;
+  if (ty1->kind == TY_FLOAT || ty2->kind == TY_FLOAT)
+    return ty_float;
+
   if (ty1->size < 4)
     ty1 = ty_int;
   if (ty2->size < 4)
