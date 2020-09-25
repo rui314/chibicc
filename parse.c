ba267f9c0938e6f970838d1c8e6fe8079365fcf1
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-26 08:09:59 +0900

    [GNU] Allow a variable as an operand of _Alignof

--- parse.c
+++ parse.c
@@ -2013,6 +2013,7 @@ static Node *funcall(Token **rest, Token *tok) {
 //         | "sizeof" "(" type-name ")"
 //         | "sizeof" unary
 //         | "_Alignof" "(" type-name ")"
+//         | "_Alignof" unary
 //         | ident func-args?
 //         | str
 //         | num
@@ -2045,11 +2046,16 @@ static Node *primary(Token **rest, Token *tok) {
     return new_num(node->ty->size, tok);
   }
 
-  if (equal(tok, "_Alignof")) {
-    tok = skip(tok->next, "(");
-    Type *ty = typename(&tok, tok);
+  if (equal(tok, "_Alignof") && equal(tok->next, "(") && is_typename(tok->next->next)) {
+    Type *ty = typename(&tok, tok->next->next);
     *rest = skip(tok, ")");
     return new_num(ty->align, tok);
+  }
+
+  if (equal(tok, "_Alignof")) {
+    Node *node = unary(rest, tok->next);
+    add_type(node);
+    return new_num(node->ty->align, tok);
   }
 
   if (tok->kind == TK_IDENT) {
