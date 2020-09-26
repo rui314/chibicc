11fb59deab250d411e3518095d0fecc335827438
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-26 11:23:04 +0900

    Add a representative node to each Node to improve error messages

--- chibicc.h
+++ chibicc.h
@@ -83,6 +83,7 @@ typedef enum {
 struct Node {
   NodeKind kind; // Node kind
   Node *next;    // Next node
+  Token *tok;    // Representative token
 
   Node *lhs;     // Left-hand side
   Node *rhs;     // Right-hand side
--- codegen.c
+++ codegen.c
@@ -31,7 +31,7 @@ static void gen_addr(Node *node) {
     return;
   }
 
-  error("not an lvalue");
+  error_tok(node->tok, "not an lvalue");
 }
 
 // Generate code for a given node.
@@ -95,7 +95,7 @@ static void gen_expr(Node *node) {
     return;
   }
 
-  error("invalid expression");
+  error_tok(node->tok, "invalid expression");
 }
 
 static void gen_stmt(Node *node) {
@@ -143,7 +143,7 @@ static void gen_stmt(Node *node) {
     return;
   }
 
-  error("invalid statement");
+  error_tok(node->tok, "invalid statement");
 }
 
 // Assign offsets to local variables.
