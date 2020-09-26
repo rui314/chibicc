a589b6af329349b963cc5b8d296f0ee71c454772
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-26 14:29:06 +0900

    Check the number of function arguments

--- parse.c
+++ parse.c
@@ -529,6 +529,9 @@ static Type *func_params(Token **rest, Token *tok, Type *ty) {
     cur = cur->next = copy_type(ty2);
   }
 
+  if (cur == &head)
+    is_variadic = true;
+
   ty = func_type(ty);
   ty->params = head.next;
   ty->is_variadic = is_variadic;
@@ -2057,6 +2060,9 @@ static Node *funcall(Token **rest, Token *tok) {
     Node *arg = assign(&tok, tok);
     add_type(arg);
 
+    if (!param_ty && !ty->is_variadic)
+      error_tok(tok, "too many arguments");
+
     if (param_ty) {
       if (param_ty->kind == TY_STRUCT || param_ty->kind == TY_UNION)
         error_tok(arg->tok, "passing struct or union is not supported yet");
@@ -2066,6 +2072,9 @@ static Node *funcall(Token **rest, Token *tok) {
 
     cur = cur->next = arg;
   }
+
+  if (param_ty)
+    error_tok(tok, "too few arguments");
 
   *rest = skip(tok, ")");
 
