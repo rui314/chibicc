c43e51239ddb46383d10b96cfc9faadd4404671c
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-27 19:45:12 +0900

    Tokenize numeric tokens as pp-numbers

--- chibicc.h
+++ chibicc.h
@@ -49,6 +49,7 @@ typedef enum {
   TK_KEYWORD, // Keywords
   TK_STR,     // String literals
   TK_NUM,     // Numeric literals
+  TK_PP_NUM,  // Preprocessing numbers
   TK_EOF,     // End-of-file markers
 } TokenKind;
 
@@ -85,7 +86,7 @@ void warn_tok(Token *tok, char *fmt, ...);
 bool equal(Token *tok, char *op);
 Token *skip(Token *tok, char *op);
 bool consume(Token **rest, Token *tok, char *str);
-void convert_keywords(Token *tok);
+void convert_pp_tokens(Token *tok);
 File **get_input_files(void);
 File *new_file(char *name, int file_no, char *contents);
 Token *tokenize(File *file);
