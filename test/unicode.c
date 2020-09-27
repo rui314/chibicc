13c340fbccc4faf14c7d444c5d4aa82312ac79b2
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-27 19:46:47 +0900

    Add UTF-16 character literal

--- test/unicode.c
+++ test/unicode.c
@@ -1,5 +1,7 @@
 #include "test.h"
 
+#define STR(x) #x
+
 int main() {
   ASSERT(4, sizeof(L'\0'));
   ASSERT(97, L'a');
@@ -13,6 +15,15 @@ int main() {
   ASSERT(946, L'β');
   ASSERT(12354, L'あ');
   ASSERT(127843, L'🍣');
+
+  ASSERT(2, sizeof(u'\0'));
+  ASSERT(1, u'\xffff'>>15);
+  ASSERT(97, u'a');
+  ASSERT(946, u'β');
+  ASSERT(12354, u'あ');
+  ASSERT(62307, u'🍣');
+
+  ASSERT(0, strcmp(STR(u'a'), "u'a'"));
 
   printf("OK\n");
   return 0;
