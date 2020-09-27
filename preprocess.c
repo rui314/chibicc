3f2c2d5bca4f4506e0ab0b03959d96be427fa672
Rui Ueyama <ruiu@cs.stanford.edu>
2020-09-27 19:45:12 +0900

    Tokenize numeric tokens as pp-numbers

--- preprocess.c
+++ preprocess.c
@@ -296,6 +296,9 @@ static long eval_const_expr(Token **rest, Token *tok) {
     }
   }
 
+  // Convert pp-numbers to regular numbers
+  convert_pp_tokens(expr);
+
   Token *rest2;
   long val = const_expr(&rest2, expr);
   if (rest2->kind != TK_EOF)
@@ -938,7 +941,7 @@ Token *preprocess(Token *tok) {
   tok = preprocess2(tok);
   if (cond_incl)
     error_tok(cond_incl->tok, "unterminated conditional directive");
-  convert_keywords(tok);
+  convert_pp_tokens(tok);
   join_adjacent_string_literals(tok);
   return tok;
 }
