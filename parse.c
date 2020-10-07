f0a018a7d6f5e3847d7e66e324c5f71a55c8b5ef
Rui Ueyama <ruiu@cs.stanford.edu>
2020-10-07 20:16:40 +0900

    Add -> operator

--- parse.c
+++ parse.c
@@ -672,7 +672,7 @@ static Node *struct_ref(Node *lhs, Token *tok) {
   return node;
 }
 
-// postfix = primary ("[" expr "]" | "." ident)*
+// postfix = primary ("[" expr "]" | "." ident | "->" ident)*
 static Node *postfix(Token **rest, Token *tok) {
   Node *node = primary(&tok, tok);
 
@@ -687,6 +687,14 @@ static Node *postfix(Token **rest, Token *tok) {
     }
 
     if (equal(tok, ".")) {
+      node = struct_ref(node, tok->next);
+      tok = tok->next->next;
+      continue;
+    }
+
+    if (equal(tok, "->")) {
+      // x->y is short for (*x).y
+      node = new_unary(ND_DEREF, node, tok);
       node = struct_ref(node, tok->next);
       tok = tok->next->next;
       continue;
