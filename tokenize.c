c2cc1d3c4500caa34da5e68eb62b7474caf96fe2
Rui Ueyama <ruiu@cs.stanford.edu>
2020-10-07 20:14:14 +0900

    Add \x<hexadecimal-sequence>

--- tokenize.c
+++ tokenize.c
@@ -79,6 +79,14 @@ static bool is_ident2(char c) {
   return is_ident1(c) || ('0' <= c && c <= '9');
 }
 
+static int from_hex(char c) {
+  if ('0' <= c && c <= '9')
+    return c - '0';
+  if ('a' <= c && c <= 'f')
+    return c - 'a' + 10;
+  return c - 'A' + 10;
+}
+
 // Read a punctuator token from p and returns its length.
 static int read_punct(char *p) {
   if (startswith(p, "==") || startswith(p, "!=") ||
@@ -108,6 +116,19 @@ static int read_escaped_char(char **new_pos, char *p) {
       if ('0' <= *p && *p <= '7')
         c = (c << 3) + (*p++ - '0');
     }
+    *new_pos = p;
+    return c;
+  }
+
+  if (*p == 'x') {
+    // Read a hexadecimal number.
+    p++;
+    if (!isxdigit(*p))
+      error_at(p, "invalid hex escape sequence");
+
+    int c = 0;
+    for (; isxdigit(*p); p++)
+      c = (c << 4) + from_hex(*p);
     *new_pos = p;
     return c;
   }
