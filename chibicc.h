07b502d4d956da489ba4daa65afa01def2d46b9b
Rui Ueyama <ruiu@cs.stanford.edu>
2020-10-07 20:19:35 +0900

    Add &, |, ^, &=, |= and ^=

--- chibicc.h
+++ chibicc.h
@@ -97,6 +97,9 @@ typedef enum {
   ND_DIV,       // /
   ND_NEG,       // unary -
   ND_MOD,       // %
+  ND_BITAND,    // &
+  ND_BITOR,     // |
+  ND_BITXOR,    // ^
   ND_EQ,        // ==
   ND_NE,        // !=
   ND_LT,        // <
