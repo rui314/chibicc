b5d061df844ecb973fa3f6ddcf498361973ff302
Rui Ueyama <ruiu@cs.stanford.edu>
2020-10-07 20:17:30 +0900

    Add +=, -=, *= and /=

--- tokenize.c
+++ tokenize.c
@@ -115,7 +115,9 @@ static int from_hex(char c) {
 
 // Read a punctuator token from p and returns its length.
 static int read_punct(char *p) {
-  static char *kw[] = {"==", "!=", "<=", ">=", "->"};
+  static char *kw[] = {
+    "==", "!=", "<=", ">=", "->", "+=", "-=", "*=", "/=",
+  };
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++)
     if (startswith(p, kw[i]))
