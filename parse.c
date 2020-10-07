daa739817c58baa8dcd0c23bb403d27d5907abfb
Rui Ueyama <ruiu@cs.stanford.edu>
2020-10-07 20:18:57 +0900

    Add % and %=

--- parse.c
+++ parse.c
@@ -697,7 +697,7 @@ static Node *to_assign(Node *binary) {
 }
 
 // assign    = equality (assign-op assign)?
-// assign-op = "=" | "+=" | "-=" | "*=" | "/="
+// assign-op = "=" | "+=" | "-=" | "*=" | "/=" | "%="
 static Node *assign(Token **rest, Token *tok) {
   Node *node = equality(&tok, tok);
 
@@ -715,6 +715,9 @@ static Node *assign(Token **rest, Token *tok) {
 
   if (equal(tok, "/="))
     return to_assign(new_binary(ND_DIV, node, assign(rest, tok->next), tok));
+
+  if (equal(tok, "%="))
+    return to_assign(new_binary(ND_MOD, node, assign(rest, tok->next), tok));
 
   *rest = tok;
   return node;
@@ -852,7 +855,7 @@ static Node *add(Token **rest, Token *tok) {
   }
 }
 
-// mul = cast ("*" cast | "/" cast)*
+// mul = cast ("*" cast | "/" cast | "%" cast)*
 static Node *mul(Token **rest, Token *tok) {
   Node *node = cast(&tok, tok);
 
@@ -866,6 +869,11 @@ static Node *mul(Token **rest, Token *tok) {
 
     if (equal(tok, "/")) {
       node = new_binary(ND_DIV, node, cast(&tok, tok->next), start);
+      continue;
+    }
+
+    if (equal(tok, "%")) {
+      node = new_binary(ND_MOD, node, cast(&tok, tok->next), start);
       continue;
     }
 
