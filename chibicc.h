3c3ab578c87e7c66021bd535f1fa8a8ba0e865fe
Rui Ueyama <ruiu@cs.stanford.edu>
2020-10-09 15:06:35 +0900

    Add -static option

--- chibicc.h
+++ chibicc.h
@@ -113,7 +113,6 @@ Token *tokenize_file(char *filename);
 //
 
 char *search_include_paths(char *filename);
-bool file_exists(char *path);
 void init_macros(void);
 void define_macro(char *name, char *buf);
 void undef_macro(char *name);
