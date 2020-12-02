4e833e5c738b3be017faea9eb1999088f7497942
0xc0 <38927632+0xc0@users.noreply.github.com>
2020-12-02 18:39:17 -0500

    Use --with-se-linux in ./configure for tinycc

--- test/thirdparty/tinycc.sh
+++ test/thirdparty/tinycc.sh
@@ -3,7 +3,7 @@ repo='https://github.com/TinyCC/tinycc.git'
 . test/thirdparty/common
 git reset --hard df67d8617b7d1d03a480a28f9f901848ffbfb7ec
 
-./configure --cc=$chibicc
+./configure --cc=$chibicc --with-selinux
 $make clean
 $make
 $make CC=cc test
